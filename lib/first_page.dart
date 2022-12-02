e73b649d353cfb986b0e070fe9cf044107833908
Yoochanhong <chanhongy6@naver.com>
2022-12-02 16:07:09 +0900

    1번 세탁기 페이지 생성

---
+++ lib/first_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
