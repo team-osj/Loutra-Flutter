dce4a5eeeab88b460b24af3d4b75b71f0ea3c8ac
Yoochanhong <chanhongy6@naver.com>
2022-12-02 15:11:44 +0900

    MainPage 생성

---
+++ lib/main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
