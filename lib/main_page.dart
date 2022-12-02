ee2b0f4063e645c350ff09eeecc21d3ba54de6a5
Yoochanhong <chanhongy6@naver.com>
2022-12-02 16:17:55 +0900

    네비게이터 성공

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:osj_flutter/first_page.dart';
+import 'package:osj_flutter/second_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -8,8 +10,28 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int selectedIndex = 0;
+  final List<Widget> pages = [FirstPage(), SecondPage()];
+
+  void IndexChange(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: pages[selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.looks_one),label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.looks_two), label: ''),
+          ],
+          currentIndex: selectedIndex,
+          selectedItemColor: Colors.lightBlue,
+          onTap: IndexChange,
+          type: BottomNavigationBarType.fixed,
+        ));
   }
 }
