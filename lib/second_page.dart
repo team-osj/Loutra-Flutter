83036829943c434d5c82be9835acfa284377c8c1
Yoochanhong <chanhongy6@naver.com>
2022-12-02 16:07:16 +0900

    2번 세탁기 페이지 생성

---
+++ lib/second_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
