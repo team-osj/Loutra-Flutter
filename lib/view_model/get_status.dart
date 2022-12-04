1964e122cf05ea675b95c37f219921ec8224959c
Yoochanhong <chanhongy6@naver.com>
2022-12-04 19:36:33 +0900

    Stream 형식으로 함수 변경

--- lib/view_model/get_status.dart
+++ lib/view_model/get_status.dart
@@ -2,12 +2,12 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:osj_flutter/model/list_model.dart';
 
-Future<OsjList> getStatus() async {
+Stream<OsjList> getStatus() async* {
   final response =
       await http.get(Uri.parse('http://pnxelec.iptime.org:1600/query.php'));
   if (response.statusCode == 200) {
     print(response.body);
-    return OsjList.fromJson(jsonDecode(response.body));
+    yield OsjList.fromJson(jsonDecode(response.body));
   } else
     throw Exception('실패');
 }
