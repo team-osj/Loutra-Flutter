0cc0e9acad910e02def43f6f249518af4a10fde6
Yoochanhong <chanhongy6@naver.com>
2022-12-04 18:35:01 +0900

    동작 여부대로 색깔 보여주기

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:osj_flutter/model/list_model.dart';
 import 'package:osj_flutter/view_model/get_status.dart';
 
@@ -25,7 +26,127 @@ class _FirstPageState extends State<FirstPage> {
         future: future,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Text('성공');
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.dry_cleaning,
+                      size: 50.0.r,
+                      color: snapshot.data!.tests![0].state == 0
+                          ? Colors.red
+                          : Colors.green,
+                    ),
+                    Icon(
+                      Icons.dry_cleaning,
+                      size: 50.0.r,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                      color: snapshot.data!.tests![1].state == 0
+                          ? Colors.red
+                          : Colors.green,
+                    ),
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                      color: snapshot.data!.tests![2].state == 0
+                          ? Colors.red
+                          : Colors.green,
+                    ),
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                      color: snapshot.data!.tests![3].state == 0
+                          ? Colors.red
+                          : Colors.green,
+                    ),
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                    ),
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                    ),
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                    ),
+                    Icon(
+                      Icons.dry_cleaning,
+                      size: 50.0.r,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(
+                      Icons.local_laundry_service_outlined,
+                      size: 50.0.r,
+                    ),
+                    Icon(
+                      Icons.dry_cleaning,
+                      size: 50.0.r,
+                    ),
+                  ],
+                ),
+              ],
+            );
           } else if (snapshot.hasError) {
             return Text(snapshot.error.toString());
           } else
