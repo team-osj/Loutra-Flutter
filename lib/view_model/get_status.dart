69dcd8a0d2c54aa680802e80a23ced45d825173f
Yoochanhong <chanhongy6@naver.com>
2022-12-04 14:11:56 +0900

    response.body 출력

--- lib/view_model/get_status.dart
+++ lib/view_model/get_status.dart
@@ -6,6 +6,7 @@ Future<Osj> getStatus() async {
   final response =
       await http.get(Uri.parse('http://pnxelec.iptime.org:1600/query.php'));
   if (response.statusCode == 200) {
+    print(response.body);
     return Osj.fromJson(jsonDecode(response.body));
   } else
     throw Exception('실패');
