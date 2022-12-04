e0a805124fd4e1734ba96b366dfcbc419e490d89
Yoochanhong <chanhongy6@naver.com>
2022-12-04 14:11:42 +0900

    list of maps 파싱용 클래스

---
+++ lib/model/list_model.dart
@@ -0,0 +1,13 @@
+import 'package:osj_flutter/model/model.dart';
+class OsjList{
+  final List<Osj>? tests;
+  OsjList({this.tests});
+  factory OsjList.fromJson(List<dynamic> json){
+    List<Osj> tests = <Osj>[];
+    tests = json.map((i) => Osj.fromJson(i)).toList();
+
+    return OsjList(
+      tests: tests,
+    );
+  }
+}
