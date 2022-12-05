003bbc767da1fc3f27eb656b170885b1d90e40f7
Yoochanhong <chanhongy6@naver.com>
2022-12-05 22:39:31 +0900

    소켓 연습용 channel 연결

--- lib/view_model/get_status.dart
+++ lib/view_model/get_status.dart
@@ -1,13 +1,9 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:osj_flutter/model/list_model.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 Stream<OsjList> getStatus() async* {
-  final response =
-      await http.get(Uri.parse('http://pnxelec.iptime.org:1600/query.php'));
-  if (response.statusCode == 200) {
-    print(response.body);
-    yield OsjList.fromJson(jsonDecode(response.body));
-  } else
-    throw Exception('실패');
+  final WebSocketChannel channel = IOWebSocketChannel.connect('http://127.0.0.1:8080/jsonData');
 }
