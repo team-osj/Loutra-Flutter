a300ab90c4cbc0c0eb191cdd62d207657e9ad150
Yoochanhong <chanhongy6@naver.com>
2022-12-06 23:39:41 +0900

    fcm 세팅(아마도?)

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,45 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:osj_flutter/main_page.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
--- lib/view_model/get_status.dart
+++ lib/view_model/get_status.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
-
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:http/http.dart' as http;
 import 'package:osj_flutter/model/list_model.dart';
 
 Stream<OsjList> getStatus() async* {
   final response =
       await http.get(Uri.parse('http://pnxelec.iptime.org:1600/query.php'));
+  var token = await FirebaseMessaging.instance.getToken();
   if (response.statusCode == 200) {
     print(response.body);
+    print(token);
     yield OsjList.fromJson(jsonDecode(response.body));
   } else
     throw Exception('실패');
