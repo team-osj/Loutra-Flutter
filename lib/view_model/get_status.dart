9bc95da70773f191baa367d04f2da6ba744fbe82
Yoochanhong <chanhongy6@naver.com>
2022-12-07 23:28:29 +0900

    스트림 => 퓨처 변경

--- lib/view_model/get_status.dart
+++ lib/view_model/get_status.dart
@@ -1,17 +1,16 @@
 import 'dart:convert';
-import 'package:firebase_messaging/firebase_messaging.dart';
+
 import 'package:http/http.dart' as http;
+import 'package:osj_flutter/baseurl.dart';
 import 'package:osj_flutter/model/list_model.dart';
 import 'package:osj_flutter/view_model/get_fcm.dart';
-import 'package:osj_flutter/baseurl.dart';
 
-Stream<OsjList> getStatus() async* {
-  final response =
-  await http.get(Uri.parse(baseurl));
+Future<OsjList> getStatus() async {
+  final response = await http.get(Uri.parse(baseurl));
   if (response.statusCode == 200) {
     print(response.body);
     await getFcm();
-    yield OsjList.fromJson(jsonDecode(response.body));
+    return OsjList.fromJson(jsonDecode(response.body));
   } else
     throw Exception('실패');
 }
