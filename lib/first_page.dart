7ba1689b0737bdef66ff0d18802322e1a8833bc0
Yoochanhong <chanhongy6@naver.com>
2022-12-08 18:31:30 +0900

    FirstPage 버튼 기능 추가

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:osj_flutter/model/list_model.dart';
+import 'package:osj_flutter/view_model/get_fcm.dart';
 
 class FirstPage extends StatelessWidget {
   FirstPage({Key? key, required this.future}) : super(key: key);
@@ -19,7 +20,9 @@ class FirstPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![0].id!.toInt());
+                    },
                     icon: Icon(Icons.dry_cleaning),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![0].state == 0
@@ -27,7 +30,9 @@ class FirstPage extends StatelessWidget {
                         : Colors.green,
                   ),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![8].id!.toInt());
+                    },
                     icon: Icon(Icons.dry_cleaning),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![8].state == 0
@@ -40,7 +45,9 @@ class FirstPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![1].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![1].state == 0
@@ -48,7 +55,9 @@ class FirstPage extends StatelessWidget {
                         : Colors.green,
                   ),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![9].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![9].state == 0
@@ -61,7 +70,9 @@ class FirstPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![2].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![2].state == 0
@@ -69,7 +80,9 @@ class FirstPage extends StatelessWidget {
                         : Colors.green,
                   ),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![10].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![10].state == 0
@@ -82,7 +95,9 @@ class FirstPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![3].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![3].state == 0
@@ -90,7 +105,9 @@ class FirstPage extends StatelessWidget {
                         : Colors.green,
                   ),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![11].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![11].state == 0
@@ -103,7 +120,9 @@ class FirstPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![4].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![4].state == 0
@@ -111,7 +130,9 @@ class FirstPage extends StatelessWidget {
                         : Colors.green,
                   ),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![12].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![12].state == 0
@@ -124,7 +145,9 @@ class FirstPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![5].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![5].state == 0
@@ -132,7 +155,9 @@ class FirstPage extends StatelessWidget {
                         : Colors.green,
                   ),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![13].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![13].state == 0
@@ -145,7 +170,9 @@ class FirstPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![6].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![6].state == 0
@@ -153,7 +180,9 @@ class FirstPage extends StatelessWidget {
                         : Colors.green,
                   ),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![14].id!.toInt());
+                    },
                     icon: Icon(Icons.dry_cleaning),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![14].state == 0
@@ -166,15 +195,19 @@ class FirstPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![7].id!.toInt());
+                    },
                     icon: Icon(Icons.local_laundry_service_outlined),
                     iconSize: 50.0.r,
-                    color: snapshot.data!.tests![6].state == 0
+                    color: snapshot.data!.tests![7].state == 0
                         ? Colors.red
                         : Colors.green,
                   ),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      getFcm(snapshot.data!.tests![15].id!.toInt());
+                    },
                     icon: Icon(Icons.dry_cleaning),
                     iconSize: 50.0.r,
                     color: snapshot.data!.tests![15].state == 0
