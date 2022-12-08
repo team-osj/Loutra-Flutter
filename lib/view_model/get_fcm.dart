66f0e1efc396011c5554fb5c375127bac4d972d6
Yoochanhong <chanhongy6@naver.com>
2022-12-08 18:31:05 +0900

    인덱스 번호마다 상태 체크

--- lib/view_model/get_fcm.dart
+++ lib/view_model/get_fcm.dart
@@ -1,14 +1,16 @@
-import 'dart:convert';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:http/http.dart' as http;
 import 'package:osj_flutter/baseurl.dart';
 
-Future<void> getFcm() async {
+Future<void> getFcm(int deviceId) async {
   var token = await FirebaseMessaging.instance.getToken();
   print(token);
-  final response = await http.get(Uri.parse(
-      '$fcmurl?token=$token&device_id=0&exp_state=1'));
+  final response =
+      await http.get(Uri.parse('$fcmurl?token=$token&device_id=$deviceId&exp_state=1'));
   if (response.statusCode == 200) {
-    print(response.body);
-  } else print('안뜸');
+    print('성공');
+    print(deviceId);
+  }
+  else
+    print('안뜸');
 }
