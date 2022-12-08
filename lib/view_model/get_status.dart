c5b1a0391e6f2ff862fb9d95b1ac9197275c50d0
Yoochanhong <chanhongy6@naver.com>
2022-12-08 09:42:05 +0900

    fcm 호출 위치 변경

--- lib/view_model/get_status.dart
+++ lib/view_model/get_status.dart
@@ -1,15 +1,12 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:osj_flutter/baseurl.dart';
 import 'package:osj_flutter/model/list_model.dart';
-import 'package:osj_flutter/view_model/get_fcm.dart';
 
 Future<OsjList> getStatus() async {
   final response = await http.get(Uri.parse(baseurl));
   if (response.statusCode == 200) {
     print(response.body);
-    await getFcm();
     return OsjList.fromJson(jsonDecode(response.body));
   } else
     throw Exception('실패');
