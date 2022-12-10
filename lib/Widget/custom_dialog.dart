1f88423739978d65e89c87cecad220c22966a1f2
Yoochanhong <chanhongy6@naver.com>
2022-12-10 17:45:09 +0900

    다이얼로그 알림 기능 함수

--- lib/Widget/custom_dialog.dart
+++ lib/Widget/custom_dialog.dart
@@ -4,68 +4,61 @@ import 'package:osj_flutter/view_model/get_fcm.dart';
 
 void showPopup(context, int deviceId) {
   showDialog(
-      context: context,
-      builder: (context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(
-              //모서리 둥글게 깎기
-              borderRadius: BorderRadius.circular(20.0)),
-          child: SizedBox(
-            width: 200.0.w,
-            height: 200.0.h,
-            child: Card(
-              elevation: 0,
-              shape: RoundedRectangleBorder(
-                  //모서리 둥글게 깎기
-                  borderRadius: BorderRadius.circular(20.0)),
-              child: Padding(
-                padding: EdgeInsets.all(30.0.r),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    context: context,
+    builder: (context) {
+      return Dialog(
+        shape: RoundedRectangleBorder(
+            //모서리 둥글게 깎기
+            borderRadius: BorderRadius.circular(20.0)),
+        child: SizedBox(
+          width: 200.0.w,
+          height: 200.0.h,
+          child: Card(
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+                //모서리 둥글게 깎기
+                borderRadius: BorderRadius.circular(20.0)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 40.0.h),
+                  child: Text(
+                    '[$deviceId]번 세탁기/건조기\n알림을 받으시겠습니까?',
+                    style: TextStyle(
+                      fontSize: 20.0.sp,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 8.0.h),
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 20.0.w),
-                        child: Text(
-                          '이 기기를 구독하시겠습니까?',
-                          style: TextStyle(
-                            fontSize: 20.0.sp,
-                          ),
-                        ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        '취소',
+                        style: TextStyle(fontSize: 15.0.sp),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 30.0.w, right: 30.0.w),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text(
-                              '취소',
-                              style: TextStyle(fontSize: 20.0.sp),
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              getFcm(deviceId);
-                            },
-                            child: Text(
-                              '확인',
-                              style: TextStyle(fontSize: 20.0.sp),
-                            ),
-                          ),
-                        ],
+                    TextButton(
+                      onPressed: () {
+                        getFcm(deviceId);
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        '확인',
+                        style: TextStyle(fontSize: 15.0.sp),
                       ),
                     ),
                   ],
                 ),
-              ),
+              ],
             ),
           ),
-        );
-      });
+        ),
+      );
+    },
+  );
 }
