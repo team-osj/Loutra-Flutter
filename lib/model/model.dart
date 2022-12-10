671e1c1dd268356a9ef58a014e91b119963d2cc3
Yoochanhong <chanhongy6@naver.com>
2022-12-10 16:41:00 +0900

    임베 기기 상태 체크용 alive 변수 추가

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,18 +1,21 @@
 class Osj {
   int? id;
   int? state;
+  int? alive;
 
-  Osj({this.id, this.state});
+  Osj({this.id, this.state, this.alive});
 
   Osj.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     state = json['state'];
+    alive = json['alive'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
     data['state'] = this.state;
+    data['alive'] = this.alive;
     return data;
   }
 }
