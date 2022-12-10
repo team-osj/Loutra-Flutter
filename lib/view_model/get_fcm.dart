a4038f9d2084422554ff890936f6386ed308ebe5
Yoochanhong <chanhongy6@naver.com>
2022-12-10 14:54:13 +0900

    fcm 받아올 준비하는 함수

--- lib/view_model/get_fcm.dart
+++ lib/view_model/get_fcm.dart
@@ -5,12 +5,11 @@ import 'package:osj_flutter/baseurl.dart';
 Future<void> getFcm(int deviceId) async {
   var token = await FirebaseMessaging.instance.getToken();
   print(token);
-  final response =
-      await http.get(Uri.parse('$fcmurl?token=$token&device_id=$deviceId&exp_state=1'));
+  final response = await http
+      .get(Uri.parse('$fcmurl?token=$token&device_id=$deviceId&exp_state=1'));
   if (response.statusCode == 200) {
     print('성공');
     print(deviceId);
-  }
-  else
+  } else
     print('안뜸');
 }
