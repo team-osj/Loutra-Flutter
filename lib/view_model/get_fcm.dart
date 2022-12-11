16c02494a2e9ee31f682b81dca685c37cf3c88ab
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-11 23:05:38 +0900

    카멜케이스

--- lib/view_model/get_fcm.dart
+++ lib/view_model/get_fcm.dart
@@ -6,7 +6,7 @@ Future<void> getFcm(int deviceId) async {
   var token = await FirebaseMessaging.instance.getToken();
   print(token);
   final response = await http
-      .get(Uri.parse('$fcmurl?token=$token&device_id=$deviceId&exp_state=1'));
+      .get(Uri.parse('$fcmUrl?token=$token&device_id=$deviceId&exp_state=1'));
   if (response.statusCode == 200) {
     print('성공');
     print(deviceId);
