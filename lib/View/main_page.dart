6cc70aec143811b4f10b2576f4e05c38c427235b
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-12 23:08:19 +0900

    적당히 UI만

--- lib/View/main_page.dart
+++ lib/View/main_page.dart
@@ -32,7 +32,18 @@ class _MainPageState extends State<MainPage>
         if (snapshot.hasData) {
           return SafeArea(
             child: Scaffold(
-              drawer: Drawer(),
+              drawer: Drawer(
+                child: ListView(
+                  children: [
+                    DrawerHeader(
+                      decoration: BoxDecoration(color: Colors.green),
+                      child: SizedBox.shrink(),
+                    ),
+                    myPageButton('문의 사항'),
+                    myPageButton('개발자 소개'),
+                  ],
+                ),
+              ),
               backgroundColor: Colors.white,
               appBar: AppBar(
                 backgroundColor: Colors.white,
@@ -40,10 +51,11 @@ class _MainPageState extends State<MainPage>
                 leading: Builder(
                   builder: (BuildContext context) {
                     return IconButton(
+                      padding: EdgeInsets.only(left: 10.0.w),
                       onPressed: () {
                         Scaffold.of(context).openDrawer();
                       },
-                      icon: Icon(Icons.menu, color: Colors.black),
+                      icon: const Icon(Icons.menu, color: Colors.black),
                     );
                   },
                 ),
@@ -113,4 +125,32 @@ class _MainPageState extends State<MainPage>
     super.dispose();
     controller!.dispose();
   }
+}
+
+Widget myPageButton(String title) {
+  return SizedBox(
+    width: 90.0.w,
+    height: 40.0.h,
+    child: MaterialButton(
+      onPressed: () {},
+      color: Colors.white,
+      elevation: 0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$title',
+            style: TextStyle(
+              color: Colors.blueAccent[400],
+              fontSize: 20.0.sp,
+            ),
+          ),
+          Icon(
+            Icons.keyboard_arrow_right_outlined,
+            color: Colors.grey,
+          ),
+        ],
+      ),
+    ),
+  );
 }
