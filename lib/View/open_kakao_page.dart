749d48d89772cce0fa3859ff3bcce4705ad4db23
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-14 10:06:56 +0900

    문의사항 오픈채팅방 웹뷰 페이지

---
+++ lib/View/open_kakao_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class OpenKakaoPage extends StatelessWidget {
+  const OpenKakaoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebView(
+          initialUrl: 'https://open.kakao.com/o/sHjnH1Se',
+          debuggingEnabled: true,
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
