d92a6f060be9740dd5a25e4e24742fd84653298a
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-15 21:42:39 +0900

    안드로이드 폰일때는 안드로이드 뷰가 보이도록 설정

--- lib/View/open_kakao_page.dart
+++ lib/View/open_kakao_page.dart
@@ -1,19 +1,28 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class OpenKakaoPage extends StatelessWidget {
+class OpenKakaoPage extends StatefulWidget {
   const OpenKakaoPage({Key? key}) : super(key: key);
 
   @override
+  State<OpenKakaoPage> createState() => _OpenKakaoPageState();
+}
+
+class _OpenKakaoPageState extends State<OpenKakaoPage> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: WebView(
-          initialUrl: 'https://open.kakao.com/o/sHjnH1Se',
-          debuggingEnabled: true,
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
-      ),
+    return WebView(
+      initialUrl: 'https://open.kakao.com/o/sHjnH1Se',
+      debuggingEnabled: true,
+      javascriptMode: JavascriptMode.unrestricted,
     );
   }
 }
