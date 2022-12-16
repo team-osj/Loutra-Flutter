9c054709d52e1c9c6425e0aa3a685fe19c2b2716
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-16 09:08:29 +0900

    icon outlined 추가

--- lib/View/icon_func.dart
+++ lib/View/icon_func.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Icon iconFunc(String type) {
   if (type == 'WASH')
-    return Icon(Icons.local_laundry_service);
+    return Icon(Icons.local_laundry_service_outlined);
   else
     return Icon(Icons.dry_cleaning);
 }
