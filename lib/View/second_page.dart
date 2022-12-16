72d8568bd3b14b14260282d0a2a8d0d7e98b0106
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-16 22:22:31 +0900

    두번째 페이지 dialog 추가

--- lib/View/second_page.dart
+++ lib/View/second_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:osj_flutter/Widget/custom_dialog.dart';
 import 'package:osj_flutter/model/list_model.dart';
+import 'package:osj_flutter/View/color_func.dart';
+import 'package:osj_flutter/View/icon_func.dart';
 
 class SecondPage extends StatelessWidget {
   SecondPage({Key? key, required this.future}) : super(key: key);
@@ -20,102 +23,18 @@ class SecondPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.dry_cleaning),
-                    iconSize: 49.0.r,
-                  ),
+                      onPressed: () {
+                        showPopup(context, 17);
+                      },
+                      icon: iconFunc(
+                          snapshot.data!.tests![16].deviceType.toString()),
+                      iconSize: 49.0.r,
+                      color:
+                          colorFunc(snapshot.data!.tests![16].state!.toInt())),
                   IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.dry_cleaning),
-                    iconSize: 49.0.r,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.local_laundry_service_outlined),
-                    iconSize: 49.0.r,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.local_laundry_service_outlined),
-                    iconSize: 49.0.r,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.local_laundry_service_outlined),
-                    iconSize: 49.0.r,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.local_laundry_service_outlined),
-                    iconSize: 49.0.r,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.local_laundry_service_outlined),
-                    iconSize: 49.0.r,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.local_laundry_service_outlined),
-                    iconSize: 49.0.r,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.local_laundry_service_outlined),
-                    iconSize: 49.0.r,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.local_laundry_service_outlined),
-                    iconSize: 49.0.r,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.local_laundry_service_outlined),
-                    iconSize: 49.0.r,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.local_laundry_service_outlined),
-                    iconSize: 49.0.r,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.dry_cleaning),
-                    iconSize: 49.0.r,
-                  ),
-                  IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      showPopup(context, 25);
+                    },
                     icon: const Icon(Icons.dry_cleaning),
                     iconSize: 49.0.r,
                   ),
@@ -125,7 +44,123 @@ class SecondPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      showPopup(context, 18);
+                    },
+                    icon: const Icon(Icons.local_laundry_service_outlined),
+                    iconSize: 49.0.r,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 26);
+                    },
+                    icon: const Icon(Icons.local_laundry_service_outlined),
+                    iconSize: 49.0.r,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 19);
+                    },
+                    icon: const Icon(Icons.local_laundry_service_outlined),
+                    iconSize: 49.0.r,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 27);
+                    },
+                    icon: const Icon(Icons.local_laundry_service_outlined),
+                    iconSize: 49.0.r,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 20);
+                    },
+                    icon: const Icon(Icons.local_laundry_service_outlined),
+                    iconSize: 49.0.r,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 28);
+                    },
+                    icon: const Icon(Icons.local_laundry_service_outlined),
+                    iconSize: 49.0.r,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 21);
+                    },
+                    icon: const Icon(Icons.local_laundry_service_outlined),
+                    iconSize: 49.0.r,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 29);
+                    },
+                    icon: const Icon(Icons.local_laundry_service_outlined),
+                    iconSize: 49.0.r,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 22);
+                    },
+                    icon: const Icon(Icons.local_laundry_service_outlined),
+                    iconSize: 49.0.r,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 30);
+                    },
+                    icon: const Icon(Icons.local_laundry_service_outlined),
+                    iconSize: 49.0.r,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 23);
+                    },
+                    icon: const Icon(Icons.dry_cleaning),
+                    iconSize: 49.0.r,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 31);
+                    },
+                    icon: const Icon(Icons.dry_cleaning),
+                    iconSize: 49.0.r,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 24);
+                    },
                     icon: const Icon(Icons.dry_cleaning),
                     iconSize: 49.0.r,
                   ),
