339b1ffacabbfdbdb87342faf7935bda98150c39
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-18 01:20:58 +0900

    초 초 경량화

--- lib/Widget/custom_icons.dart
+++ lib/Widget/custom_icons.dart
@@ -6,6 +6,7 @@ class CustomIcons {
   static const _kFontFam = 'CustomIcons';
   static const String? _kFontPkg = null;
 
-  static const IconData tumble_dryer = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData washing_machine = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tumble_dryer = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData triangle_up = IconData(0xf311, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
