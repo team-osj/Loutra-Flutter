c73d90966f2bfdc8b1f1882a4a4b807f79c9420a
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-18 01:10:20 +0900

    건조기 아이콘 변경

--- lib/View/icon_func.dart
+++ lib/View/icon_func.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:osj_flutter/Widget/custom_icons.dart';
 
 Icon iconFunc(String type) {
   if (type == 'WASH') {
     return const Icon(Icons.local_laundry_service_outlined);
   } else {
-    return const Icon(Icons.dry_cleaning);
+    return const Icon(CustomIcons.tumble_dryer);
   }
 }
