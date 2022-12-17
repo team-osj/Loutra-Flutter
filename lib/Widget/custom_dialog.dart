eb37a982cf548ddaf3a1d4f3e4dcc61b6e89baf8
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-17 11:47:40 +0900

    다이얼로그 UI 일부 변경

--- lib/Widget/custom_dialog.dart
+++ lib/Widget/custom_dialog.dart
@@ -5,59 +5,45 @@ import 'package:osj_flutter/view_model/get_fcm.dart';
 void showPopup(context, int deviceId) {
   showDialog(
     context: context,
-    builder: (context) {
-      return Dialog(
+    builder: (BuildContext context) {
+      return AlertDialog(
         shape: RoundedRectangleBorder(
             //모서리 둥글게 깎기
-            borderRadius: BorderRadius.circular(20.0)),
-        child: SizedBox(
-          width: 200.0.w,
-          height: 180.0.h,
-          child: Card(
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-                //모서리 둥글게 깎기
-                borderRadius: BorderRadius.circular(20.0)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 40.0.h),
-                  child: Text(
-                    '[$deviceId]번 세탁기/건조기\n알림을 받으시겠습니까?',
-                    style: TextStyle(
-                      fontSize: 20.0.sp,
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(
-                        '취소',
-                        style: TextStyle(fontSize: 15.0.sp),
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        getFcm(deviceId);
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(
-                        '확인',
-                        style: TextStyle(fontSize: 15.0.sp),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+            borderRadius: BorderRadius.circular(10.0)),
+        title: Column(
+          children: [
+            Text(
+              '[$deviceId]번 세탁기/건조기\n알림을 받으시겠습니까?',
+              style: TextStyle(fontSize: 22.0.sp),
             ),
-          ),
+          ],
         ),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  '취소',
+                  style: TextStyle(fontSize: 15.0.sp),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  getFcm(deviceId);
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  '확인',
+                  style: TextStyle(fontSize: 15.0.sp),
+                ),
+              ),
+            ],
+          ),
+        ],
       );
     },
   );
