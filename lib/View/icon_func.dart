14a017a24df74fbfee41320f05bb9c643ec01155
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-18 01:22:13 +0900

    커스텀 세탁기 아이콘으로 교체

--- lib/View/icon_func.dart
+++ lib/View/icon_func.dart
@@ -3,7 +3,7 @@ import 'package:osj_flutter/Widget/custom_icons.dart';
 
 Icon iconFunc(String type) {
   if (type == 'WASH') {
-    return const Icon(Icons.local_laundry_service_outlined);
+    return const Icon(CustomIcons.washing_machine);
   } else {
     return const Icon(CustomIcons.tumble_dryer);
   }
