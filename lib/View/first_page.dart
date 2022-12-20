ab973fd4f95f2661b5412fdeb9342e1634526b47
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-20 22:18:07 +0900

    ListView.builder 간격 이슈로 롤백 ㅠㅠ

--- lib/View/first_page.dart
+++ lib/View/first_page.dart
@@ -16,41 +16,202 @@ class FirstPage extends StatelessWidget {
       future: future,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: 8,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          showPopup(context, index + 1);
-                        },
-                        icon: iconFunc(snapshot.data!.tests![index].deviceType
-                            .toString()),
-                        iconSize: 49.0.r,
-                        color: colorFunc(
-                            snapshot.data!.tests![index].state!.toInt())),
-                    const Icon(CustomIcons.triangle_up, color: Colors.grey),
-                    IconButton(
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
                       onPressed: () {
-                        showPopup(context, index + 9);
+                        showPopup(context, 1);
                       },
-                      icon: iconFunc(snapshot
-                          .data!.tests![index + 9].deviceType
-                          .toString()),
+                      icon: iconFunc(
+                          snapshot.data!.tests![0].deviceType.toString()),
                       iconSize: 49.0.r,
-                      color: colorFunc(
-                          snapshot.data!.tests![index + 9].state!.toInt()),
-                    ),
-                  ],
-                ),
-              );
-            },
+                      color:
+                          colorFunc(snapshot.data!.tests![0].state!.toInt())),
+                  const Icon(CustomIcons.triangle_up, color: Colors.grey),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 9);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![8].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![8].state!.toInt()),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 2);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![1].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![1].state!.toInt()),
+                  ),
+                  const Icon(CustomIcons.triangle_up, color: Colors.grey),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 10);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![9].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![9].state!.toInt()),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 3);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![2].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![2].state!.toInt()),
+                  ),
+                  const Icon(CustomIcons.triangle_up, color: Colors.grey),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 11);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![10].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![10].state!.toInt()),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 4);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![3].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![3].state!.toInt()),
+                  ),
+                  const Icon(CustomIcons.triangle_up, color: Colors.grey),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 12);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![11].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![11].state!.toInt()),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 5);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![4].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![4].state!.toInt()),
+                  ),
+                  const Icon(CustomIcons.triangle_up, color: Colors.grey),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 13);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![12].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![12].state!.toInt()),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 6);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![5].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![5].state!.toInt()),
+                  ),
+                  const Icon(CustomIcons.triangle_up, color: Colors.grey),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 14);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![13].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![13].state!.toInt()),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 7);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![6].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![6].state!.toInt()),
+                  ),
+                  const Icon(CustomIcons.triangle_up, color: Colors.grey),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 15);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![14].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![14].state!.toInt()),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 8);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![7].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![7].state!.toInt()),
+                  ),
+                  const Icon(CustomIcons.triangle_up, color: Colors.grey),
+                  IconButton(
+                    onPressed: () {
+                      showPopup(context, 16);
+                    },
+                    icon: iconFunc(
+                        snapshot.data!.tests![15].deviceType.toString()),
+                    iconSize: 49.0.r,
+                    color: colorFunc(snapshot.data!.tests![15].state!.toInt()),
+                  ),
+                ],
+              ),
+            ],
           );
         } else if (snapshot.hasError) {
           return Text(snapshot.error.toString());
