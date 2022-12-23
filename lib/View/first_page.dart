2ca808da1ce4c6cd28edd2d82e17dfd7f64a04fc
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-24 00:14:34 +0900

    유지보수 쉽게 디바이스 id로 변경

--- lib/View/first_page.dart
+++ lib/View/first_page.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:osj_flutter/Widget/show_popup.dart';
-import 'package:osj_flutter/Widget/custom_icons.dart';
 import 'package:osj_flutter/model/list_model.dart';
-import 'package:osj_flutter/View/color_func.dart';
-import 'package:osj_flutter/View/icon_func.dart';
+import 'package:osj_flutter/Widget/custom_row_buttons.dart';
 
 class FirstPage extends StatelessWidget {
   FirstPage({Key? key, required this.future}) : super(key: key);
@@ -21,66 +17,66 @@ class FirstPage extends StatelessWidget {
             children: [
               customRowButtons(
                   context,
-                  '1',
+                  snapshot.data!.tests![0].id.toString(),
                   snapshot.data!.tests![0].deviceType.toString(),
                   snapshot.data!.tests![0].state!.toInt(),
-                  ' 9',
+                  ' ${snapshot.data!.tests![8].id}',
                   snapshot.data!.tests![8].deviceType.toString(),
                   snapshot.data!.tests![8].state!.toInt()),
               customRowButtons(
                   context,
-                  '2',
+                  snapshot.data!.tests![1].id.toString(),
                   snapshot.data!.tests![1].deviceType.toString(),
                   snapshot.data!.tests![1].state!.toInt(),
-                  '10',
+                  snapshot.data!.tests![9].id.toString(),
                   snapshot.data!.tests![9].deviceType.toString(),
                   snapshot.data!.tests![9].state!.toInt()),
               customRowButtons(
                   context,
-                  '3',
+                  snapshot.data!.tests![2].id.toString(),
                   snapshot.data!.tests![2].deviceType.toString(),
                   snapshot.data!.tests![2].state!.toInt(),
-                  '11',
+                  snapshot.data!.tests![10].id.toString(),
                   snapshot.data!.tests![10].deviceType.toString(),
                   snapshot.data!.tests![10].state!.toInt()),
               customRowButtons(
                   context,
-                  '4',
+                  snapshot.data!.tests![3].id.toString(),
                   snapshot.data!.tests![3].deviceType.toString(),
                   snapshot.data!.tests![3].state!.toInt(),
-                  '12',
+                  snapshot.data!.tests![11].id.toString(),
                   snapshot.data!.tests![11].deviceType.toString(),
                   snapshot.data!.tests![11].state!.toInt()),
               customRowButtons(
                   context,
-                  '5',
+                  snapshot.data!.tests![4].id.toString(),
                   snapshot.data!.tests![4].deviceType.toString(),
                   snapshot.data!.tests![4].state!.toInt(),
-                  '13',
+                  snapshot.data!.tests![12].id.toString(),
                   snapshot.data!.tests![12].deviceType.toString(),
                   snapshot.data!.tests![12].state!.toInt()),
               customRowButtons(
                   context,
-                  '6',
+                  snapshot.data!.tests![5].id.toString(),
                   snapshot.data!.tests![5].deviceType.toString(),
                   snapshot.data!.tests![5].state!.toInt(),
-                  '14',
+                  snapshot.data!.tests![13].id.toString(),
                   snapshot.data!.tests![13].deviceType.toString(),
                   snapshot.data!.tests![13].state!.toInt()),
               customRowButtons(
                   context,
-                  '7',
+                  snapshot.data!.tests![6].id.toString(),
                   snapshot.data!.tests![6].deviceType.toString(),
                   snapshot.data!.tests![6].state!.toInt(),
-                  '15',
+                  snapshot.data!.tests![14].id.toString(),
                   snapshot.data!.tests![14].deviceType.toString(),
                   snapshot.data!.tests![14].state!.toInt()),
               customRowButtons(
                   context,
-                  '8',
+                  snapshot.data!.tests![7].id.toString(),
                   snapshot.data!.tests![7].deviceType.toString(),
                   snapshot.data!.tests![7].state!.toInt(),
-                  '16',
+                  snapshot.data!.tests![15].id.toString(),
                   snapshot.data!.tests![15].deviceType.toString(),
                   snapshot.data!.tests![15].state!.toInt()),
             ],
@@ -91,73 +87,6 @@ class FirstPage extends StatelessWidget {
           return const Center(child: CircularProgressIndicator());
         }
       },
-    );
-  }
-
-  customRowButtons(BuildContext context, String a, String aDeviceType,
-      int aState, String b, String bDeviceType, int bState) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        GestureDetector(
-          onTap: () {
-            showPopup(context, a);
-          },
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(20.0)),
-            width: 110.0.w,
-            height: 60.0.h,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  a.toString(),
-                  style: TextStyle(fontSize: 25.0.sp),
-                ),
-                IconButton(
-                    onPressed: () {
-                      showPopup(context, a);
-                    },
-                    icon: iconFunc(aDeviceType),
-                    iconSize: 40.0.r,
-                    color: colorFunc(aState)),
-              ],
-            ),
-          ),
-        ),
-        const Icon(CustomIcons.triangle_up, color: Colors.grey),
-        GestureDetector(
-          onTap: () {
-            showPopup(context, b);
-          },
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(20.0)),
-            width: 110.0.w,
-            height: 60.0.h,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  b.toString(),
-                  style: TextStyle(fontSize: 25.0.sp),
-                ),
-                IconButton(
-                  onPressed: () {
-                    showPopup(context, b);
-                  },
-                  icon: iconFunc(bDeviceType),
-                  iconSize: 40.0.r,
-                  color: colorFunc(bState),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
