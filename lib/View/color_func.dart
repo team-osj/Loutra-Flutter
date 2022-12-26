81e551ba6d6fdc6d4bbfcf945a469b93758b40c3
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-26 22:50:45 +0900

    죽었을때 색깔 빨간색으로 변경

--- lib/View/color_func.dart
+++ lib/View/color_func.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:osj_flutter/Widget/custom_colors.dart';
 
-Color? colorFunc(int state) {
-  if (state == 0) {
+Color? colorFunc(int state, int alive) {
+  if (state == 0 || alive == 0) {
     return customRed;
   } else {
     return customGreen;
