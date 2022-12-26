48fa8ec480ddb56c5e081cd6984abc7f67479d4b
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-26 23:20:10 +0900

    alive 0일때 dialog 문구 변경

--- lib/Widget/show_popup.dart
+++ lib/Widget/show_popup.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:osj_flutter/view_model/get_fcm.dart';
 
-void showPopup(context, String deviceId) {
+void showPopup(context, String deviceId, int alive) {
   showDialog(
     context: context,
     builder: (BuildContext context) {
@@ -10,39 +10,56 @@ void showPopup(context, String deviceId) {
         shape: RoundedRectangleBorder(
             //모서리 둥글게 깎기
             borderRadius: BorderRadius.circular(10.0)),
-        title: Column(
-          children: [
-            Text(
-              '[$deviceId]번 세탁기/건조기\n알림을 받으시겠습니까?',
-              style: TextStyle(fontSize: 22.0.sp),
-            ),
-          ],
-        ),
+        title: alive == 1
+            ? Text(
+                '[$deviceId]번 세탁기/건조기\n알림을 받으시겠습니까?',
+                style: TextStyle(fontSize: 22.0.sp),
+              )
+            : Column(
+                children: [
+                  Text('불편을 드려 죄송합니다.', style: TextStyle(fontSize: 22.0.sp)),
+                  Text('현재 $deviceId번 세탁기/건조기를',
+                      style: TextStyle(fontSize: 22.0.sp)),
+                  Text('사용할 수 없습니다.', style: TextStyle(fontSize: 22.0.sp)),
+                ],
+              ),
         actions: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text(
-                  '취소',
-                  style: TextStyle(fontSize: 15.0.sp),
+          alive == 1
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        '취소',
+                        style: TextStyle(fontSize: 15.0.sp),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        getFcm(deviceId);
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        '확인',
+                        style: TextStyle(fontSize: 15.0.sp),
+                      ),
+                    ),
+                  ],
+                )
+              : Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      '확인',
+                      style: TextStyle(fontSize: 15.0.sp),
+                    ),
+                  ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  getFcm(deviceId);
-                  Navigator.of(context).pop();
-                },
-                child: Text(
-                  '확인',
-                  style: TextStyle(fontSize: 15.0.sp),
-                ),
-              ),
-            ],
-          ),
         ],
       );
     },
