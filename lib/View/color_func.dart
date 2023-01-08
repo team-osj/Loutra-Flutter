3b17a7b1217fc71adf2866a9b1c44512d8b1aafd
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-08 20:34:06 +0900

    분기처리 안된 색깔 포함 변경

--- lib/View/color_func.dart
+++ lib/View/color_func.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:osj_flutter/Widget/custom_colors.dart';
 
 Color? colorFunc(int state, int alive) {
-  if (state == 0) {
-    return customRed;
-  } else if (alive == 0) {
+  if (alive == 0) {
     return Colors.black;
+  } else if (state == 0) {
+    return customRed;
   } else {
     return customGreen;
   }
