54ab82d0ee4edb725585f989fec55249bc69f9d1
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-16 22:14:25 +0900

    쿼리 수정

--- lib/view_model/get_status.dart
+++ lib/view_model/get_status.dart
@@ -5,7 +5,7 @@ import 'package:osj_flutter/baseurl.dart';
 import 'package:osj_flutter/model/list_model.dart';
 
 Future<OsjList> getStatus() async {
-  final response = await http.get(Uri.parse(baseurl));
+  final response = await http.get(Uri.parse('$baseurl/query.php'));
   if (response.statusCode == 200) {
     print(response.body);
     return OsjList.fromJson(jsonDecode(response.body));
