5bc530e95378a5b24896f251b83d1d0c1d5cb53e
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-21 02:21:04 +0900

    factory 키워드 사용

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -6,19 +6,12 @@ class Osj {
 
   Osj({this.id, this.state, this.deviceType, this.alive});
 
-  Osj.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    state = json['state'];
-    deviceType = json['device_type'];
-    alive = json['alive'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['state'] = state;
-    data['device_type'] = deviceType;
-    data['alive'] = alive;
-    return data;
+  factory Osj.fromJson(Map<String, dynamic> json) {
+    return Osj(
+      id: json['id'],
+      state: json['state'],
+      deviceType: json['deviceType'],
+      alive: json['alive'],
+    );
   }
 }
