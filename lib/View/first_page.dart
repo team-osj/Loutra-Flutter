1a074746af75953699f3732408af1506d8ec38f6
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-23 17:39:26 +0900

    Streambuilder => 데이터를 수신 후 Column으로 뿌려줌

--- lib/View/first_page.dart
+++ lib/View/first_page.dart
@@ -3,114 +3,103 @@ import 'package:osj_flutter/model/list_model.dart';
 import 'package:osj_flutter/Widget/custom_row_buttons.dart';
 
 class FirstPage extends StatelessWidget {
-  FirstPage({Key? key, required this.stream}) : super(key: key);
-  Stream<OsjList>? stream;
+  FirstPage({Key? key, required this.osjList}) : super(key: key);
+  OsjList osjList;
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: stream,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              customRowButtons(
-                context,
-                snapshot.data!.tests![0].id.toString(),
-                snapshot.data!.tests![0].deviceType.toString(),
-                snapshot.data!.tests![0].state!.toInt(),
-                snapshot.data!.tests![0].alive!.toInt(),
-                ' ${snapshot.data!.tests![8].id}',
-                snapshot.data!.tests![8].deviceType.toString(),
-                snapshot.data!.tests![8].state!.toInt(),
-                snapshot.data!.tests![8].alive!.toInt(),
-              ),
-              customRowButtons(
-                context,
-                snapshot.data!.tests![1].id.toString(),
-                snapshot.data!.tests![1].deviceType.toString(),
-                snapshot.data!.tests![1].state!.toInt(),
-                snapshot.data!.tests![1].alive!.toInt(),
-                snapshot.data!.tests![9].id.toString(),
-                snapshot.data!.tests![9].deviceType.toString(),
-                snapshot.data!.tests![9].state!.toInt(),
-                snapshot.data!.tests![9].alive!.toInt(),
-              ),
-              customRowButtons(
-                context,
-                snapshot.data!.tests![2].id.toString(),
-                snapshot.data!.tests![2].deviceType.toString(),
-                snapshot.data!.tests![2].state!.toInt(),
-                snapshot.data!.tests![2].alive!.toInt(),
-                snapshot.data!.tests![10].id.toString(),
-                snapshot.data!.tests![10].deviceType.toString(),
-                snapshot.data!.tests![10].state!.toInt(),
-                snapshot.data!.tests![10].alive!.toInt(),
-              ),
-              customRowButtons(
-                context,
-                snapshot.data!.tests![3].id.toString(),
-                snapshot.data!.tests![3].deviceType.toString(),
-                snapshot.data!.tests![3].state!.toInt(),
-                snapshot.data!.tests![3].alive!.toInt(),
-                snapshot.data!.tests![11].id.toString(),
-                snapshot.data!.tests![11].deviceType.toString(),
-                snapshot.data!.tests![11].state!.toInt(),
-                snapshot.data!.tests![11].alive!.toInt(),
-              ),
-              customRowButtons(
-                context,
-                snapshot.data!.tests![4].id.toString(),
-                snapshot.data!.tests![4].deviceType.toString(),
-                snapshot.data!.tests![4].state!.toInt(),
-                snapshot.data!.tests![4].alive!.toInt(),
-                snapshot.data!.tests![12].id.toString(),
-                snapshot.data!.tests![12].deviceType.toString(),
-                snapshot.data!.tests![12].state!.toInt(),
-                snapshot.data!.tests![12].alive!.toInt(),
-              ),
-              customRowButtons(
-                context,
-                snapshot.data!.tests![5].id.toString(),
-                snapshot.data!.tests![5].deviceType.toString(),
-                snapshot.data!.tests![5].state!.toInt(),
-                snapshot.data!.tests![5].alive!.toInt(),
-                snapshot.data!.tests![13].id.toString(),
-                snapshot.data!.tests![13].deviceType.toString(),
-                snapshot.data!.tests![13].state!.toInt(),
-                snapshot.data!.tests![13].alive!.toInt(),
-              ),
-              customRowButtons(
-                context,
-                snapshot.data!.tests![6].id.toString(),
-                snapshot.data!.tests![6].deviceType.toString(),
-                snapshot.data!.tests![6].state!.toInt(),
-                snapshot.data!.tests![6].alive!.toInt(),
-                snapshot.data!.tests![14].id.toString(),
-                snapshot.data!.tests![14].deviceType.toString(),
-                snapshot.data!.tests![14].state!.toInt(),
-                snapshot.data!.tests![14].alive!.toInt(),
-              ),
-              customRowButtons(
-                context,
-                snapshot.data!.tests![7].id.toString(),
-                snapshot.data!.tests![7].deviceType.toString(),
-                snapshot.data!.tests![7].state!.toInt(),
-                snapshot.data!.tests![7].alive!.toInt(),
-                snapshot.data!.tests![15].id.toString(),
-                snapshot.data!.tests![15].deviceType.toString(),
-                snapshot.data!.tests![15].state!.toInt(),
-                snapshot.data!.tests![15].alive!.toInt(),
-              ),
-            ],
-          );
-        } else if (snapshot.hasError) {
-          return Text(snapshot.error.toString());
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        customRowButtons(
+          context,
+          osjList.tests![0].id.toString(),
+          osjList.tests![0].deviceType.toString(),
+          osjList.tests![0].state.toInt(),
+          osjList.tests![0].alive.toInt(),
+          ' ${osjList.tests![8].id}',
+          osjList.tests![8].deviceType.toString(),
+          osjList.tests![8].state.toInt(),
+          osjList.tests![8].alive.toInt(),
+        ),
+        customRowButtons(
+          context,
+          osjList.tests![1].id.toString(),
+          osjList.tests![1].deviceType.toString(),
+          osjList.tests![1].state.toInt(),
+          osjList.tests![1].alive.toInt(),
+          osjList.tests![9].id.toString(),
+          osjList.tests![9].deviceType.toString(),
+          osjList.tests![9].state.toInt(),
+          osjList.tests![9].alive.toInt(),
+        ),
+        customRowButtons(
+          context,
+          osjList.tests![2].id.toString(),
+          osjList.tests![2].deviceType.toString(),
+          osjList.tests![2].state.toInt(),
+          osjList.tests![2].alive.toInt(),
+          osjList.tests![10].id.toString(),
+          osjList.tests![10].deviceType.toString(),
+          osjList.tests![10].state.toInt(),
+          osjList.tests![10].alive.toInt(),
+        ),
+        customRowButtons(
+          context,
+          osjList.tests![3].id.toString(),
+          osjList.tests![3].deviceType.toString(),
+          osjList.tests![3].state.toInt(),
+          osjList.tests![3].alive.toInt(),
+          osjList.tests![11].id.toString(),
+          osjList.tests![11].deviceType.toString(),
+          osjList.tests![11].state.toInt(),
+          osjList.tests![11].alive.toInt(),
+        ),
+        customRowButtons(
+          context,
+          osjList.tests![4].id.toString(),
+          osjList.tests![4].deviceType.toString(),
+          osjList.tests![4].state.toInt(),
+          osjList.tests![4].alive.toInt(),
+          osjList.tests![12].id.toString(),
+          osjList.tests![12].deviceType.toString(),
+          osjList.tests![12].state.toInt(),
+          osjList.tests![12].alive.toInt(),
+        ),
+        customRowButtons(
+          context,
+          osjList.tests![5].id.toString(),
+          osjList.tests![5].deviceType.toString(),
+          osjList.tests![5].state.toInt(),
+          osjList.tests![5].alive.toInt(),
+          osjList.tests![13].id.toString(),
+          osjList.tests![13].deviceType.toString(),
+          osjList.tests![13].state.toInt(),
+          osjList.tests![13].alive.toInt(),
+        ),
+        customRowButtons(
+          context,
+          osjList.tests![6].id.toString(),
+          osjList.tests![6].deviceType.toString(),
+          osjList.tests![6].state.toInt(),
+          osjList.tests![6].alive.toInt(),
+          osjList.tests![14].id.toString(),
+          osjList.tests![14].deviceType.toString(),
+          osjList.tests![14].state.toInt(),
+          osjList.tests![14].alive.toInt(),
+        ),
+        customRowButtons(
+          context,
+          osjList.tests![7].id.toString(),
+          osjList.tests![7].deviceType.toString(),
+          osjList.tests![7].state.toInt(),
+          osjList.tests![7].alive.toInt(),
+          osjList.tests![15].id.toString(),
+          osjList.tests![15].deviceType.toString(),
+          osjList.tests![15].state.toInt(),
+          osjList.tests![15].alive.toInt(),
+        ),
+      ],
     );
   }
 }
