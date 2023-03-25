965ed474ecf10f1bc8e036d2f8800ebf6d863c59
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-25 20:48:22 +0900

    옵셔널 제거

--- lib/View/color_func.dart
+++ lib/View/color_func.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:osj_flutter/Widget/custom_colors.dart';
 
-Color? colorFunc(int state, int alive) {
+Color colorFunc(int state, int alive) {
   if (alive == 0) {
     return Colors.black;
   } else if (state == 0) {
--- lib/Widget/custom_colors.dart
+++ lib/Widget/custom_colors.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-Color? customGreen = const Color(0xFF03CE5B);
-Color? customRed = const Color(0xFFF80012);
+Color customGreen = const Color(0xFF03CE5B);
+Color customRed = const Color(0xFFF80012);
