ee3658f98bac5e95563b4008f02facc52001dcbc
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-28 19:39:10 +0900

    onConnect함수 제거

--- lib/view_model/get_fcm.dart
+++ lib/view_model/get_fcm.dart
@@ -9,15 +9,11 @@ Future<void> getFcm(String deviceId) async {
     'device_id': deviceId,
     'expect_state': '1',
   };
+  print('$deviceId번 기기 fcm 전송');
+  socket.emit('request_push', deviceStatus);
   socket.onConnect((data) {
-    print('$deviceId번 기기 fcm 전송');
-    socket.emit('request_push', deviceStatus);
   });
   socket.on('msg', (data) {
     print('답장 : $data');
   });
-  socket.onDisconnect((_) => print('disconnect'));
-  socket.onConnectError((data) => print('ConnectError : $data'));
-  socket.onError((data) => print('Error : $data'));
-  socket.connect();
 }
