c0835a49d9bbffd4fff74f950b1cc8003985019c
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-13 20:46:32 +0900

    삭제

--- android/app/src/main/kotlin/com/team/osj/Lotura/lotura_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.team.osj.Lotura.lotura_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/Global/socket.dart
+++
@@ -1,9 +0,0 @@
-import 'package:socket_io_client/socket_io_client.dart' as IO;
-import 'package:lotura_flutter/baseurl.dart';
-
-IO.Socket socket = IO.io(
-    '$baseurl/application',
-    IO.OptionBuilder()
-        .setTransports(['websocket'])
-        .enableForceNewConnection()
-        .build());
