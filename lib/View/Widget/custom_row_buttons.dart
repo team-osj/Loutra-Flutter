b8dc569dc8ffbe1c7dcb3cef1811f32d7145af8c
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-13 18:13:02 +0900

    커스텀 가로 버튼

---
+++ lib/View/Widget/custom_row_buttons.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:lotura_flutter/View/Widget/custom_icons.dart';
+import 'package:lotura_flutter/View/Widget/show_popup.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lotura_flutter/View/icon_func.dart';
+import 'package:lotura_flutter/View/color_func.dart';
+
+customRowButton(BuildContext context, String a, String aDeviceType, int aState,
+    int aAlive, String b, String bDeviceType, int bState, int bAlive) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: [
+      GestureDetector(
+        onTap: () {
+          showPopup(context, a, aAlive, a);
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(20.0)),
+          width: 110.0.w,
+          height: 60.0.h,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                a.toString(),
+                style: TextStyle(
+                    fontSize: 20.0.sp,
+                    fontFamily: 'NanumGothicCoding',
+                    fontWeight: FontWeight.bold),
+              ),
+              IconButton(
+                onPressed: () {
+                  showPopup(context, a, aAlive, aDeviceType);
+                },
+                icon: iconFunc(aDeviceType, aAlive),
+                iconSize: 40.0.r,
+                color: colorFunc(aState, aAlive),
+              ),
+            ],
+          ),
+        ),
+      ),
+      const Icon(CustomIcons.triangleUp, color: Colors.grey),
+      GestureDetector(
+        onTap: () {
+          showPopup(context, b, bAlive, bDeviceType);
+        },
+        child: b == 'null'
+            ? SizedBox(
+                width: 110.0.w,
+                height: 60.0.h,
+              )
+            : Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                width: 110.0.w,
+                height: 60.0.h,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      b.toString(),
+                      style: TextStyle(
+                          fontSize: 20.0.sp,
+                          fontFamily: 'NanumGothicCoding',
+                          fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        showPopup(context, b, bAlive, bDeviceType);
+                      },
+                      icon: iconFunc(bDeviceType, bAlive),
+                      iconSize: 40.0.r,
+                      color: colorFunc(bState, bAlive),
+                    ),
+                  ],
+                ),
+              ),
+      ),
+    ],
+  );
+}
