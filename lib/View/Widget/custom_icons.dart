e3737b51114b61a3e8f44734f966ddfe5ec970cd
맥북 유찬홍 <chanhongy6@naver.com>
2023-04-13 14:22:38 +0900

    커스텀으로 쓸 아이콘 이름 정의

---
+++ lib/View/Widget/custom_icons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData washingMachine =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tumbleDryer =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData triangleUp =
+      IconData(0xf311, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
