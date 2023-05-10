2e6700ace9a33043a5bd0bfb1834dd6bfbee0c17
yoochanhong <chanhongy6@naver.com>
2023-05-10 23:14:18 +0900

    스트림에 리스트 더해주기

--- lib/service/receive_apply_list.dart
+++ lib/service/receive_apply_list.dart
@@ -1,12 +1,17 @@
+import 'dart:async';
 import 'package:lotura/global/socket.dart';
+import 'package:lotura/model/apply_list.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-Future<void> receiveApplyList(String token) async {
+
+void receiveApplyList(StreamController streamController) async {
+  var token = await FirebaseMessaging.instance.getToken();
   var deviceToken = {
     'token': token,
   };
   print('리스트 받아오기');
   socket.emit('view_request', deviceToken);
   socket.on('request_list', (data) {
-    print(data);
+    streamController.sink.add(ApplyList.fromJson(data));
   });
 }
