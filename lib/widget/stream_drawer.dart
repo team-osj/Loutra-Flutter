d99267b52fd29849727302f40c1ae53dab609673
yoochanhong <chanhongy6@naver.com>
2023-05-10 23:13:40 +0900

    드로어 클래스 생성

---
+++ lib/widget/stream_drawer.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:lotura/model/apply_list.dart';
+import 'package:lotura/service/receive_apply_list.dart';
+
+class StreamDrawer extends StatefulWidget {
+  StreamDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<StreamDrawer> createState() => _StreamDrawerState();
+}
+
+class _StreamDrawerState extends State<StreamDrawer> {
+  late StreamController<ApplyList> controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = StreamController<ApplyList>();
+    receiveApplyList(controller);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: StreamBuilder<ApplyList>(
+          stream: controller.stream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.applyList!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                        snapshot.data!.applyList![index].deviceId.toString()),
+                  );
+                },
+              );
+            }
+            return const Center(child: CircularProgressIndicator());
+          }),
+    );
+  }
+}
