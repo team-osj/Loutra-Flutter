5024fc2a996bee9c3e8e0fa168dba03658b73240
yoochanhong <chanhongy6@naver.com>
2023-06-30 23:11:00 +0900

    feat :: OS별 Dialog 함수로 분리

--- lib/widget/show_popup.dart
+++ lib/widget/show_popup.dart
@@ -11,142 +11,9 @@ void showPopup(
     context: context,
     builder: (BuildContext context) {
       if (Theme.of(context).platform == TargetPlatform.android) {
-        return AlertDialog(
-          insetPadding: EdgeInsets.zero,
-          shape: RoundedRectangleBorder(
-              //모서리 둥글게 깎기
-              borderRadius: BorderRadius.circular(10.0)),
-          title: state == 0
-              ? alive == 1
-                  ? Column(
-                      children: [
-                        Text('$deviceId번 $type 알림을',
-                            style: TextStyle(fontSize: 22.0.sp)),
-                        Text(
-                          '받으시겠습니까?',
-                          style: TextStyle(fontSize: 22.0.sp),
-                        ),
-                      ],
-                    )
-                  : Column(
-                      children: [
-                        Text('불편을 드려 죄송합니다.',
-                            style: TextStyle(
-                                fontSize: 22.0.sp,
-                                fontWeight: FontWeight.bold)),
-                        Text('현재 $deviceId번 $type를',
-                            style: TextStyle(fontSize: 22.0.sp)),
-                        Text('사용할 수 없습니다.',
-                            style: TextStyle(fontSize: 22.0.sp)),
-                      ],
-                    )
-              : Column(
-                  children: [
-                    Text('$type를 사용할 수 있는',
-                        style: TextStyle(fontSize: 22.0.sp)),
-                    Text(
-                      '상태입니다.',
-                      style: TextStyle(fontSize: 22.0.sp),
-                    ),
-                  ],
-                ),
-          actions: [
-            state == 0
-                ? alive == 1
-                    ? Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text(
-                              '취소',
-                              style: TextStyle(fontSize: 15.0.sp),
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              sendFcmInfo(deviceId);
-                              Navigator.of(context).pop();
-                            },
-                            child: Text(
-                              '확인',
-                              style: TextStyle(fontSize: 15.0.sp),
-                            ),
-                          ),
-                        ],
-                      )
-                    : Center(
-                        child: TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text(
-                            '확인',
-                            style: TextStyle(fontSize: 15.0.sp),
-                          ),
-                        ),
-                      )
-                : Center(
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(
-                        '확인',
-                        style: TextStyle(fontSize: 15.0.sp),
-                      ),
-                    ),
-                  ),
-          ],
-        );
+        return androidDialog(state, alive, deviceId, type, context);
       } else {
-        return CupertinoAlertDialog(
-          title: state == 0
-              ? alive == 1
-                  ? Column(
-                      children: [
-                        Text('$deviceId번 $type 알림을',
-                            style: TextStyle(fontSize: 20.0.sp)),
-                        Text(
-                          '받으시겠습니까?',
-                          style: TextStyle(fontSize: 20.0.sp),
-                        ),
-                      ],
-                    )
-                  : Column(
-                      children: [
-                        Text('불편을 드려 죄송합니다.',
-                            style: TextStyle(fontSize: 20.0.sp)),
-                        Text('현재 $deviceId번 $type를',
-                            style: TextStyle(fontSize: 20.0.sp)),
-                        Text('사용할 수 없습니다.',
-                            style: TextStyle(fontSize: 20.0.sp)),
-                      ],
-                    )
-              : Column(
-                  children: [
-                    Text('$type를 사용할 수 있는',
-                        style: TextStyle(fontSize: 20.0.sp)),
-                    Text(
-                      '상태입니다.',
-                      style: TextStyle(fontSize: 20.0.sp),
-                    ),
-                  ],
-                ),
-          actions: state == 0 && alive == 1
-              ? apply(context, deviceId)
-              : [
-                  CupertinoDialogAction(
-                    child: Text(
-                      'OK',
-                      style: TextStyle(fontSize: 18.0.sp),
-                    ),
-                    onPressed: () => Navigator.of(context).pop(),
-                  )
-                ],
-        );
+        return iOSDialog(state, alive, deviceId, type, context);
       }
     },
   );
@@ -176,4 +43,141 @@ List<Widget> apply(BuildContext context, String deviceId) {
     ),
   );
   return list;
+}
+
+AlertDialog androidDialog(
+    int state, alive, String deviceId, type, BuildContext context) {
+  return AlertDialog(
+    insetPadding: EdgeInsets.zero,
+    shape: RoundedRectangleBorder(
+        //모서리 둥글게 깎기
+        borderRadius: BorderRadius.circular(10.0)),
+    title: state == 0
+        ? alive == 1
+            ? Column(
+                children: [
+                  Text('$deviceId번 $type 알림을',
+                      style: TextStyle(fontSize: 22.0.sp)),
+                  Text(
+                    '받으시겠습니까?',
+                    style: TextStyle(fontSize: 22.0.sp),
+                  ),
+                ],
+              )
+            : Column(
+                children: [
+                  Text('불편을 드려 죄송합니다.',
+                      style: TextStyle(
+                          fontSize: 22.0.sp, fontWeight: FontWeight.bold)),
+                  Text('현재 $deviceId번 $type를',
+                      style: TextStyle(fontSize: 22.0.sp)),
+                  Text('사용할 수 없습니다.', style: TextStyle(fontSize: 22.0.sp)),
+                ],
+              )
+        : Column(
+            children: [
+              Text('$type를 사용할 수 있는', style: TextStyle(fontSize: 22.0.sp)),
+              Text(
+                '상태입니다.',
+                style: TextStyle(fontSize: 22.0.sp),
+              ),
+            ],
+          ),
+    actions: [
+      state == 0
+          ? alive == 1
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        '취소',
+                        style: TextStyle(fontSize: 15.0.sp),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        sendFcmInfo(deviceId);
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        '확인',
+                        style: TextStyle(fontSize: 15.0.sp),
+                      ),
+                    ),
+                  ],
+                )
+              : Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      '확인',
+                      style: TextStyle(fontSize: 15.0.sp),
+                    ),
+                  ),
+                )
+          : Center(
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  '확인',
+                  style: TextStyle(fontSize: 15.0.sp),
+                ),
+              ),
+            ),
+    ],
+  );
+}
+
+CupertinoAlertDialog iOSDialog(
+    int state, alive, String deviceId, type, BuildContext context) {
+  return CupertinoAlertDialog(
+    title: state == 0
+        ? alive == 1
+            ? Column(
+                children: [
+                  Text('$deviceId번 $type 알림을',
+                      style: TextStyle(fontSize: 20.0.sp)),
+                  Text(
+                    '받으시겠습니까?',
+                    style: TextStyle(fontSize: 20.0.sp),
+                  ),
+                ],
+              )
+            : Column(
+                children: [
+                  Text('불편을 드려 죄송합니다.', style: TextStyle(fontSize: 20.0.sp)),
+                  Text('현재 $deviceId번 $type를',
+                      style: TextStyle(fontSize: 20.0.sp)),
+                  Text('사용할 수 없습니다.', style: TextStyle(fontSize: 20.0.sp)),
+                ],
+              )
+        : Column(
+            children: [
+              Text('$type를 사용할 수 있는', style: TextStyle(fontSize: 20.0.sp)),
+              Text(
+                '상태입니다.',
+                style: TextStyle(fontSize: 20.0.sp),
+              ),
+            ],
+          ),
+    actions: state == 0 && alive == 1
+        ? apply(context, deviceId)
+        : [
+            CupertinoDialogAction(
+              child: Text(
+                'OK',
+                style: TextStyle(fontSize: 18.0.sp),
+              ),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
+  );
 }
