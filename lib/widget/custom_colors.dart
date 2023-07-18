b694839c3b6e9de6b1601569cd3ec1793bfdc917
yoochanhong <chanhongy6@naver.com>
2023-07-18 21:57:33 +0900

    feat :: 커스텀 색상 정의

--- lib/widget/custom_colors.dart
+++ lib/widget/custom_colors.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
-// Color customGreen = const Color(0xFF03CE5B);
-// Color customRed = const Color(0xFFF80012);
+Color primary900 = const Color(0xff164ED4);
+Color primary700 = const Color(0xff3C70EC);
+Color primary500 = const Color(0xff7599ED);
+Color primary300 = const Color(0xff98B1ED);
+Color primary100 = const Color(0xffEBF1FF);
+Color primary50 = const Color(0xffFAFBFF);
 
-Color customGreen = const Color(0xFF7DB45A);
-Color customRed = const Color(0xFFDA6156);
+Color green700 = const Color(0xff25BD1D);
+Color green100 = const Color(0xffECFFEB);
+Color green50 = const Color(0xffF8FFF8);
+
+Color red700 = const Color(0xffD91F1F);
+Color red100 = const Color(0xffFFEBEB);
+Color red50 = const Color(0xffFFFDFD);
+
+Color black = const Color(0xff000000);
+Color gray900 = const Color(0xff1D1F22);
+Color gray700 = const Color(0xff2E3135);
+Color gray500 = const Color(0xff676C74);
+Color gray300 = const Color(0xffADB3BD);
+Color gray100 = const Color(0xffF0F3F6);
+Color white = const Color(0xffFFFFFF);
