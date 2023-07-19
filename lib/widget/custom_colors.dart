7797d700a4cd2fe147b560cf662ef4a03cf9400d
yoochanhong <chanhongy6@naver.com>
2023-07-19 12:51:48 +0900

    feat :: change class
    static 필드로 변경하였습니다

--- lib/widget/custom_colors.dart
+++ lib/widget/custom_colors.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 
-Color primary900 = const Color(0xff164ED4);
-Color primary700 = const Color(0xff3C70EC);
-Color primary500 = const Color(0xff7599ED);
-Color primary300 = const Color(0xff98B1ED);
-Color primary100 = const Color(0xffEBF1FF);
-Color primary50 = const Color(0xffFAFBFF);
+class OsjColor {
+  static Color customGreen = const Color(0xFF7DB45A);
+  static Color customRed = const Color(0xFFDA6156);
 
-Color green700 = const Color(0xff25BD1D);
-Color green100 = const Color(0xffECFFEB);
-Color green50 = const Color(0xffF8FFF8);
+  static Color primary900 = const Color(0xff164ED4);
+  static Color primary700 = const Color(0xff3C70EC);
+  static Color primary500 = const Color(0xff7599ED);
+  static Color primary300 = const Color(0xff98B1ED);
+  static Color primary100 = const Color(0xffEBF1FF);
+  static Color primary50 = const Color(0xffFAFBFF);
 
-Color red700 = const Color(0xffD91F1F);
-Color red100 = const Color(0xffFFEBEB);
-Color red50 = const Color(0xffFFFDFD);
+  static Color green700 = const Color(0xff25BD1D);
+  static Color green100 = const Color(0xffECFFEB);
+  static Color green50 = const Color(0xffF8FFF8);
 
-Color black = const Color(0xff000000);
-Color gray900 = const Color(0xff1D1F22);
-Color gray700 = const Color(0xff2E3135);
-Color gray500 = const Color(0xff676C74);
-Color gray300 = const Color(0xffADB3BD);
-Color gray100 = const Color(0xffF0F3F6);
-Color white = const Color(0xffFFFFFF);
+  static Color red700 = const Color(0xffD91F1F);
+  static Color red100 = const Color(0xffFFEBEB);
+  static Color red50 = const Color(0xffFFFDFD);
+
+  static Color black = const Color(0xff000000);
+  static Color gray900 = const Color(0xff1D1F22);
+  static Color gray700 = const Color(0xff2E3135);
+  static Color gray500 = const Color(0xff676C74);
+  static Color gray300 = const Color(0xffADB3BD);
+  static Color gray100 = const Color(0xffF0F3F6);
+  static Color white = const Color(0xffFFFFFF);
+}
