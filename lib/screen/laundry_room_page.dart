18ed2ae26ce8addfd530e5e80419ec8d15146c56
yoochanhong <chanhongy6@naver.com>
2023-07-19 23:03:34 +0900

    feat :: 앱바 퍼블리싱

--- lib/screen/laundry_room_page.dart
+++ lib/screen/laundry_room_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lotura/widget/custom_colors.dart';
+import 'package:lotura/widget/osj_icons.dart';
+import 'package:lotura/widget/osj_text_button.dart';
 
 class LaundryRoomPage extends StatefulWidget {
   const LaundryRoomPage({super.key});
@@ -13,6 +16,31 @@ class _LaundryRoomPageState extends State<LaundryRoomPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: OsjColor.gray100,
+      appBar: AppBar(
+        backgroundColor: OsjColor.gray100,
+        elevation: 0.0,
+        leadingWidth: 300.0.w,
+        leading: Row(
+          children: [
+            SizedBox(width: 24.0.w),
+            Text(
+              '남자 기숙사측 세탁실',
+              style: TextStyle(
+                color: OsjColor.black,
+                fontSize: 24.0.sp,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings, color: OsjColor.black),
+          ),
+          SizedBox(width: 24.0.w),
+        ],
+      ),
     );
   }
 }
