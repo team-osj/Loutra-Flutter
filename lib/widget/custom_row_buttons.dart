7e54ebddb310303ce44e997409946bd4497920e7
yoochanhong <chanhongy6@naver.com>
2023-07-20 16:19:38 +0900

    feat :: change class

--- lib/widget/custom_row_buttons.dart
+++ lib/widget/custom_row_buttons.dart
@@ -1,81 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:lotura/Widget/custom_icons.dart';
-import 'package:lotura/Widget/show_popup.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:lotura/widget/icon_func.dart';
-import 'package:lotura/widget/color_func.dart';
+import 'package:lotura/widget/machine_button.dart';
+import 'package:lotura/main.dart';
 
-customRowButtons(BuildContext context, String a, String aDeviceType, int aState,
-    int aAlive, String b, String bDeviceType, int bState, int bAlive) {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceAround,
-    children: [
-      GestureDetector(
-        onTap: () {
-          showPopup(context, a, aAlive, aState, a);
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 1, color: Colors.grey),
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          width: 110.0.w,
-          height: 60.0.h,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                a.toString(),
-                style: TextStyle(fontSize: 20.0.sp, fontFamily: 'Inter'),
-              ),
-              IconButton(
-                onPressed: () {
-                  showPopup(context, a, aAlive, aState, aDeviceType);
-                },
-                icon: iconFunc(aDeviceType, aAlive),
-                iconSize: 40.0.r,
-                color: colorFunc(aState, aAlive),
-              ),
-            ],
-          ),
-        ),
-      ),
-      const Icon(CustomIcons.triangleUp, color: Colors.grey),
-      GestureDetector(
-        onTap: () {
-          showPopup(context, b, bAlive, bState, bDeviceType);
-        },
-        child: b == 'null'
-            ? SizedBox(
-                width: 110.0.w,
-                height: 60.0.h,
-              )
-            : Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1, color: Colors.grey),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                width: 110.0.w,
-                height: 60.0.h,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      b.toString(),
-                      style: TextStyle(fontSize: 20.0.sp, fontFamily: 'Inter'),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        showPopup(context, b, bAlive, bState, bDeviceType);
-                      },
-                      icon: iconFunc(bDeviceType, bAlive),
-                      iconSize: 40.0.r,
-                      color: colorFunc(bState, bAlive),
-                    ),
-                  ],
-                ),
-              ),
-      ),
-    ],
-  );
+class CustomRowButton extends StatelessWidget {
+  const CustomRowButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        MachineButton(
+            index: 10,
+            status: Status.breakdown,
+            machine: Machine.laundryMachine),
+        const Icon(CustomIcons.triangleUp, color: Colors.grey),
+        MachineButton(
+            index: 9, status: Status.working, machine: Machine.dryMachine),
+      ],
+    );
+  }
 }
+
+//customRowButtons(
+//         BuildContext context,
+//         String aIndex,
+//         String aDeviceType,
+//         int aState,
+//         int aAlive,
+//         String bIndex,
+//         String bDeviceType,
+//         int bState,
+//         int bAlive) =>
+//     Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: [
+//         MachineButton(
+//             index: 10,
+//             status: Status.breakdown,
+//             machine: Machine.laundryMachine),
+//         const Icon(CustomIcons.triangleUp, color: Colors.grey),
+//         MachineButton(
+//             index: 9, status: Status.working, machine: Machine.dryMachine),
+//       ],
+//     );
