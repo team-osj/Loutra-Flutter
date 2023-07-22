f98c1b4e36b9184b5f6e1a7af57015ce01a9ff41
yoochanhong <chanhongy6@naver.com>
2023-07-22 15:22:07 +0900

    feat :: 알림 신청, 삭제 여부 필드 추가

--- lib/widget/machine_button.dart
+++ lib/widget/machine_button.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lotura/main.dart';
 import 'package:lotura/widget/custom_colors.dart';
+import 'package:lotura/widget/osj_bottom_sheet.dart';
 import 'package:lotura/widget/osj_icons.dart';
-import 'package:lotura/widget/osj_text_button.dart';
 
 class MachineButton extends StatelessWidget {
   MachineButton({
     super.key,
     required this.index,
+    required this.isEnableNotification,
     required this.status,
     required this.machine,
   });
 
   final int index;
+  final bool isEnableNotification;
   final Status status;
   final Machine machine;
 
@@ -50,53 +52,11 @@ class MachineButton extends StatelessWidget {
               top: Radius.circular(25.r),
             ),
           ),
-          builder: (context) => SizedBox(
-            width: double.infinity,
-            height: 220.0.h,
-            child: Padding(
-              padding: EdgeInsets.only(
-                left: 24.0.w,
-                right: 24.0.w,
-                top: 32.0.h,
-                bottom: 12.0.h,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '$index번 ${machineText[machine]}의\n알림 설정을 해제하실건가요?',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 22.0.sp,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  SizedBox(height: 24.0.h),
-                  Row(
-                    children: [
-                      OSJTextButton(
-                          function: ()=> Navigator.of(context).pop(),
-                          width: 185.0.w,
-                          height: 56.0.h,
-                          fontSize: 16.0.h,
-                          color: OsjColor.gray100,
-                          fontColor: OsjColor.black,
-                          text: "취소"),
-                      SizedBox(width: 12.0.w),
-                      OSJTextButton(
-                          function: () => Navigator.of(context).pop(),
-                          width: 185.0.w,
-                          height: 56.0.h,
-                          fontSize: 16.0.h,
-                          color: OsjColor.primary700,
-                          fontColor: OsjColor.white,
-                          text: "알림 해제"),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
+          builder: (context) => OSJBottomSheet(
+              index: index,
+              isEnableNotification: isEnableNotification,
+              status: status,
+              machine: machine),
         );
       },
       child: Container(
