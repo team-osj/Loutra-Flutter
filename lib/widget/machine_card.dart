a5e0e1baee215974cb8bdd0a0d89ed3d9df97cb3
yoochanhong <chanhongy6@naver.com>
2023-07-22 22:06:43 +0900

    feat :: machine card 32번 기기 예외처리

--- lib/widget/machine_card.dart
+++ lib/widget/machine_card.dart
@@ -20,7 +20,7 @@ class MachineCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return (!isWoman && index == 32) || (isWoman && index == -1)
+    return (!isWoman && index == 31) || (isWoman && index == -1)
         ? SizedBox(width: 185.0.w, height: 256.0.h)
         : GestureDetector(
             onTap: () => showModalBottomSheet(
