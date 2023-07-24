c72ac021acaf4798045e3568434f774559c06445
yoochanhong <chanhongy6@naver.com>
2023-07-24 22:39:43 +0900

    create :: setting page 생성

---
+++ lib/screen/setting_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
