0fa7b7ba3aee05fe6ed4e54ff9ea4c850ea30351
yoochanhong <chanhongy6@naver.com>
2023-07-24 23:27:39 +0900

    feat :: 앱바 퍼블리싱

--- lib/screen/setting_page.dart
+++ lib/screen/setting_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lotura/widget/custom_colors.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: OsjColor.gray100,
+      appBar: AppBar(
+        backgroundColor: OsjColor.gray100,
+        elevation: 0.0,
+        leadingWidth: 300.0.w,
+        leading: Row(
+          children: [
+            IconButton(
+              padding: EdgeInsets.only(left: 24.0.w, right: 12.0.w),
+              onPressed: () => Navigator.pop(context),
+              icon: Icon(
+                Icons.keyboard_arrow_left,
+                color: OsjColor.black,
+                size: 24.0.w,
+              ),
+            ),
+            Text(
+              "설정",
+              style: TextStyle(fontSize: 24.0.sp, color: OsjColor.black),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
