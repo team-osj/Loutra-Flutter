cbd8ff4644f7085bae298cb91dda3945163d7697
yoochanhong <chanhongy6@naver.com>
2023-07-25 15:38:57 +0900

    feat :: 설정 페이지 바텀시트 적용

--- lib/screen/setting_page.dart
+++ lib/screen/setting_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lotura/widget/custom_colors.dart';
+import 'package:lotura/widget/setting_page_bottom_sheet.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
@@ -46,55 +47,30 @@ class SettingPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    "화면 테마",
-                    style: TextStyle(fontSize: 16.0.sp),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "밝은 모드",
-                        style: TextStyle(
-                          fontSize: 16.0.sp,
-                          color: OsjColor.primary700,
-                        ),
-                      ),
-                      SizedBox(width: 12.0.w),
-                      Icon(
-                        Icons.keyboard_arrow_right,
-                        color: OsjColor.gray300,
-                        size: 24.0.r,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 12.0.h),
-            Padding(
-              padding: EdgeInsets.all(12.0.r),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
                     "메인 세탁실 설정",
                     style: TextStyle(fontSize: 16.0.sp),
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        "남자 기숙사측",
-                        style: TextStyle(
-                          fontSize: 16.0.sp,
-                          color: OsjColor.primary700,
+                  GestureDetector(
+                    onTap: () => showModalBottomSheet(
+                        context: context,
+                        builder: (context) => SettingPageBottomSheet()),
+                    child: Row(
+                      children: [
+                        Text(
+                          "남자 기숙사측",
+                          style: TextStyle(
+                            fontSize: 16.0.sp,
+                            color: OsjColor.primary700,
+                          ),
                         ),
-                      ),
-                      SizedBox(width: 12.0.w),
-                      Icon(
-                        Icons.keyboard_arrow_right,
-                        color: OsjColor.gray300,
-                        size: 24.0.r,
-                      ),
-                    ],
+                        SizedBox(width: 12.0.w),
+                        Icon(
+                          Icons.keyboard_arrow_right,
+                          color: OsjColor.gray300,
+                          size: 24.0.r,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
