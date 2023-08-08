c976cb94cf931377d369e1c8a9fc911a54cf1f83
yoochanhong <chanhongy6@naver.com>
2023-08-08 15:12:53 +0900

    feat :: init 메소드 분리
    알림 신청 이후 다시 보내기 메소드를 실행할 수 있도록
    보내기와 받기 메소드로 따로 분리했습니다.

--- lib/data/repository/apply_repository.dart
+++ lib/data/repository/apply_repository.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:lotura/secret.dart';
 import 'package:lotura/domain/model/apply_response_list.dart';
 import 'package:lotura/global/socket.dart';
+import 'package:socket_io_client/socket_io_client.dart';
 
 class ApplyRepository {
   final StreamController<ApplyResponseList> _streamController;
@@ -13,15 +15,32 @@ class ApplyRepository {
 
   ApplyRepository(this._streamController);
 
-  Future<Map<String, String>> _getToken() async {
+  Future<String> _getToken() async {
     var token = await FirebaseMessaging.instance.getToken();
-    var deviceToken = {'token': token.toString()};
-    return deviceToken;
+    return token.toString();
   }
 
-  void init() {
-    _getToken().then((value) => socket.emit(sendRequestApplyList, value));
-    socket.on(receiveResponseApplyList,
-        (data) => _streamController.add(ApplyResponseList.fromJson(data)));
+  void applyListRequest() {
+    _getToken().then((value) => socket.emit(
+          sendRequestApplyList,
+          {'token': value},
+        ));
+  }
+
+  void response() {
+    socket.on(receiveResponseApplyList, (data) {
+      _streamController.sink.add(ApplyResponseList.fromJson(data));
+    });
+  }
+
+  void sendFCMInfo(int deviceId) {
+    print('$deviceId번 기기 fcm 전송');
+    _getToken().then((value) {
+      socket.emit(sendFCM, {
+        'token': value,
+        'device_id': deviceId,
+        'expect_state': '1',
+      });
+    });
   }
 }
