fa661036934169ed4feb1782735f785b9e582cf4
yoochanhong <chanhongy6@naver.com>
2023-08-08 19:40:31 +0900

    style :: 수정된 파일 경로 적용

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ import 'package:lotura/domain/model/osj_list.dart';
 import 'package:lotura/presentation/utils/osj_colors.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lotura/domain/model/apply_response_list.dart';
-import 'package:lotura/data/repository/apply_repository.dart';
-import 'package:lotura/data/repository/laundry_repository.dart';
 import 'package:lotura/domain/repository/apply_repository.dart';
 import 'package:lotura/domain/repository/laundry_repository.dart';
+import 'package:lotura/data/repository/apply_repository_impl.dart';
 import 'package:lotura/presentation/splash_page/bloc/osj_bloc.dart';
+import 'package:lotura/data/repository/laundry_repository_impl.dart';
 import 'package:lotura/presentation/splash_page/bloc/apply_bloc.dart';
 import 'package:lotura/presentation/splash_page/ui/view/splash_page.dart';
 
