fd6690461b3146963c30e1943ead2e0beec38b4e
yoochanhong <chanhongy6@naver.com>
2023-08-08 15:11:50 +0900

    feat :: 알림 신청 핸들러 추가
    알림 신청 후 바로 리스트를 새로고침하게 될 경우 서버에서 요청을 처리하고 완료 하기 전 리스트를 불러오게 됩니다.
    강제적으로 0.5초 딜레이를 줌으로써 문제를 해결했습니다.

--- lib/presentation/splash_page/bloc/apply_bloc.dart
+++ lib/presentation/splash_page/bloc/apply_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:lotura/presentation/splash_page/bloc/apply_event.dart';
 import 'package:lotura/presentation/splash_page/bloc/apply_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,11 +11,23 @@ class ApplyBloc extends Bloc<ApplyEvent, ApplyState> {
   ApplyBloc(this._repository) : super(Empty()) {
     on<GetApplyListEvent>((event, emit) async {
       try {
-        _repository.init();
         emit(Loading());
+        _repository.applyListRequest();
+        _repository.response();
         await for (var value in _repository.applyStream) {
           emit(Loaded(applyList: value));
         }
+      } catch (e) {
+        emit(Error(message: e.toString()));
+      }
+    });
+    on<SendFCMEvent>((event, emit) async {
+      try {
+        emit(Loading());
+        _repository.sendFCMInfo(event.deviceId);
+        Future.delayed(const Duration(milliseconds: 500))
+            .then((value) => _repository.applyListRequest());
+        emit(SendCompletion());
       } catch (e) {
         emit(Error(message: e.toString()));
       }
