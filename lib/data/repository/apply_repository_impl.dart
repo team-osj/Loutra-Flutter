f8620d808c0b9396f833ef40d600f33bdd910b55
yoochanhong <chanhongy6@naver.com>
2023-08-12 01:14:50 +0900

    feat :: 지역 소켓 인스턴스 사용

--- lib/data/repository/apply_repository_impl.dart
+++ lib/data/repository/apply_repository_impl.dart
@@ -1,13 +1,20 @@
 import 'dart:async';
 
 import 'package:lotura/secret.dart';
-import 'package:lotura/global/socket.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:lotura/domain/model/apply_response_list.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:lotura/domain/repository/apply_repository.dart';
 
 class ApplyRepositoryImpl implements ApplyRepository {
   final StreamController<ApplyResponseList> _streamController;
+
+  IO.Socket socket = IO.io(
+      '$baseurl/application',
+      IO.OptionBuilder()
+          .setTransports(['websocket'])
+          .enableForceNewConnection()
+          .build());
 
   @override
   Stream<ApplyResponseList> get applyStream =>
