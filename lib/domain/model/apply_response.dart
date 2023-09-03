2656a483ee56c4a791ff0bfd3ca15dd8cbf25067
yoochanhong <chanhongy6@naver.com>
2023-09-03 18:46:15 +0900

    feat :: state 필드 추가

--- lib/domain/model/apply_response.dart
+++ lib/domain/model/apply_response.dart
@@ -1,13 +1,16 @@
 class ApplyResponse {
   final int deviceId;
   final String deviceType;
+  final int state;
 
-  ApplyResponse({required this.deviceId, required this.deviceType});
+  ApplyResponse(
+      {required this.deviceId, required this.deviceType, required this.state});
 
   factory ApplyResponse.fromJson(Map<String, dynamic> json) {
     return ApplyResponse(
       deviceId: json['device_id'],
       deviceType: json['device_type'],
+      state: json['state'],
     );
   }
 }
