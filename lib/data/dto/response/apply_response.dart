6c612a3fc8572794bbe868ecea396479b20bb818
yoochanhong <chanhongy6@naver.com>
2023-09-22 22:10:52 +0900

    알림 신청 결과 response dto 추가

---
+++ lib/data/dto/response/apply_response.dart
@@ -0,0 +1,13 @@
+class ApplyResponse {
+  final int deviceId;
+  final String deviceType;
+
+  ApplyResponse({required this.deviceId, required this.deviceType});
+
+  factory ApplyResponse.fromJson(Map<String, dynamic> json) {
+    return ApplyResponse(
+      deviceId: json['device_id'],
+      deviceType: json['device_type'],
+    );
+  }
+}
