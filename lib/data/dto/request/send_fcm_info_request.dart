040fbf794e3793c0cbf21045f5bee29cf3f695f3
yoochanhong <chanhongy6@naver.com>
2023-09-22 22:31:17 +0900

    fcm 정보 보내기 request dto 추가

---
+++ lib/data/dto/request/send_fcm_info_request.dart
@@ -0,0 +1,16 @@
+class SendFCMInfoRequest {
+  String? token;
+  int deviceId;
+  String expectState;
+
+  SendFCMInfoRequest(
+      {this.token, required this.deviceId, required this.expectState});
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['token'] = token;
+    data['device_id'] = deviceId;
+    data['expect_state'] = expectState;
+    return data;
+  }
+}
