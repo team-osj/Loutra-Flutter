12dbb849ae377eb789d084edc76d40fdfca41613
yoochanhong <chanhongy6@naver.com>
2023-09-24 19:24:14 +0900

    refactor :: repository => usecase 변경

--- lib/presentation/laundry_room_page/bloc/laundry_bloc.dart
+++ lib/presentation/laundry_room_page/bloc/laundry_bloc.dart
@@ -1,22 +1,27 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:lotura/domain/repository/laundry_repository.dart';
+import 'package:lotura/domain/use_case/get_laundry_status_use_case.dart';
 import 'package:lotura/presentation/laundry_room_page/bloc/laundry_event.dart';
 import 'package:lotura/presentation/laundry_room_page/bloc/laundry_state.dart';
 
 class LaundryBloc extends Bloc<LaundryEvent, LaundryState> {
-  final LaundryRepository _repository;
+  final GetLaundryStatusUseCase _getLaundryStatusUseCase;
 
-  LaundryBloc(this._repository) : super(Empty()) {
-    on<GetLaundryEvent>((event, emit) async {
-      try {
-        _repository.init();
-        emit(Loading());
-        await for (var value in _repository.laundryList) {
-          emit(Loaded(laundryResponseList: value));
-        }
-      } catch (e) {
-        emit(Error(message: e.toString()));
+  LaundryBloc({required GetLaundryStatusUseCase getLaundryStatusUseCase})
+      : _getLaundryStatusUseCase = getLaundryStatusUseCase,
+        super(Empty()) {
+    on<GetLaundryEvent>(_getLaundryEventHandler);
+  }
+
+  void _getLaundryEventHandler(
+      GetLaundryEvent event, Emitter<LaundryState> emit) async {
+    try {
+      _getLaundryStatusUseCase.execute();
+      emit(Loading());
+      await for (var value in _getLaundryStatusUseCase.laundryList) {
+        emit(Loaded(laundryResponseList: value));
       }
-    });
+    } catch (e) {
+      emit(Error(message: e.toString()));
+    }
   }
 }
