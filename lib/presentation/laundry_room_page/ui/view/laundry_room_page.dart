887703f8d9c04cf4e4dbbdcc25ab5c0289638274
yoochanhong <chanhongy6@naver.com>
2023-10-20 14:13:09 +0900

    refactor :: LaundryRoomPage LoturaIcons 리팩토링

--- lib/presentation/laundry_room_page/ui/view/laundry_room_page.dart
+++ lib/presentation/laundry_room_page/ui/view/laundry_room_page.dart
@@ -6,8 +6,8 @@ import 'package:lotura/presentation/laundry_room_page/bloc/laundry_bloc.dart';
 import 'package:lotura/presentation/laundry_room_page/bloc/laundry_state.dart';
 import 'package:lotura/presentation/setting_page/ui/view/setting_page.dart';
 import 'package:lotura/presentation/utils/custom_row_buttons.dart';
+import 'package:lotura/presentation/utils/lotura_icons.dart';
 import 'package:lotura/presentation/utils/osj_colors.dart';
-import 'package:lotura/presentation/utils/osj_icons.dart';
 import 'package:lotura/presentation/utils/osj_text_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -151,7 +151,7 @@ class _LaundryRoomPageState extends State<LaundryRoomPage> {
                       isSelectedIcon = 0;
                     });
                   },
-                  icon: Icon(OSJIcons.grid,
+                  icon: Icon(LoturaIcons.grid,
                       size: 18.0.r,
                       color: isSelectedIcon == 0
                           ? OSJColors.black
@@ -164,7 +164,7 @@ class _LaundryRoomPageState extends State<LaundryRoomPage> {
                       });
                     },
                     icon: Icon(
-                      OSJIcons.list,
+                      LoturaIcons.list,
                       size: 18.0.r,
                       color: isSelectedIcon == 1
                           ? OSJColors.black
