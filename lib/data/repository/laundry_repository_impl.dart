fb0ef0b246ddc4edbe5240b66a07c07b176ccc4b
yoochanhong <chanhongy6@naver.com>
2024-01-06 16:40:46 +0900

    feat :: remote, local laundry data source 의존성 추가

--- lib/data/repository/laundry_repository_impl.dart
+++ lib/data/repository/laundry_repository_impl.dart
@@ -1,40 +1,26 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:lotura/data/data_source/laundry/local/local_laundry_data_source.dart';
+import 'package:lotura/data/data_source/laundry/remote/remote_laundry_data_source.dart';
 import 'package:lotura/data/dto/response/laundry_response.dart';
 import 'package:lotura/domain/repository/laundry_repository.dart';
-import 'package:lotura/secret.dart';
-import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class LaundryRepositoryImpl implements LaundryRepository {
-  final StreamController<List<LaundryResponse>> _streamController;
   final LocalLaundryDataSource _localLaundryDataSource;
+  final RemoteLaundryDataSource _remoteLaundryDataSource;
 
-  IO.Socket socket = IO.io(
-      '$baseurl/application',
-      IO.OptionBuilder()
-          .setTransports(['websocket'])
-          .enableForceNewConnection()
-          .build());
+  LaundryRepositoryImpl(
+      {required LocalLaundryDataSource localLaundryDataSource,
+      required RemoteLaundryDataSource remoteLaundryDataSource})
+      : _localLaundryDataSource = localLaundryDataSource,
+        _remoteLaundryDataSource = remoteLaundryDataSource;
 
   @override
   Stream<List<LaundryResponse>> get laundryList =>
-      _streamController.stream.asBroadcastStream();
-
-  LaundryRepositoryImpl(this._streamController, this._localLaundryDataSource);
+      _remoteLaundryDataSource.laundryList.asBroadcastStream();
 
   @override
-  void init() {
-    socket.onConnect((data) => debugPrint("연결 성공"));
-    socket.on('update', (data) {
-      List<LaundryResponse> laundryList = List.empty(growable: true);
-      laundryList = (data as List<dynamic>)
-          .map((i) => LaundryResponse.fromJson(i))
-          .toList();
-      _streamController.sink.add(laundryList);
-    });
-  }
+  void init() => _remoteLaundryDataSource.init();
 
   @override
   V? getValue<V>({required String key}) =>
