625d8146d6784f6c5bccda3daaacfcc43076d63a
yoochanhong <chanhongy6@naver.com>
2024-01-25 15:33:33 +0900

    feat :: 알림 신청 취소 use case 로직 변경
    서버에서 취소된 리스트를 리턴해줌

--- lib/domain/use_case/apply_cancel_use_case.dart
+++ lib/domain/use_case/apply_cancel_use_case.dart
@@ -1,5 +1,5 @@
 import 'package:lotura/data/dto/request/apply_cancel_request.dart';
-import 'package:lotura/data/dto/request/get_apply_list_request.dart';
+import 'package:lotura/data/dto/response/apply_response.dart';
 import 'package:lotura/domain/repository/apply_repository.dart';
 
 class ApplyCancelUseCase {
@@ -8,9 +8,7 @@ class ApplyCancelUseCase {
   ApplyCancelUseCase({required ApplyRepository applyRepository})
       : _applyRepository = applyRepository;
 
-  void execute(ApplyCancelRequest applyCancelRequest) {
-    _applyRepository.applyCancel(applyCancelRequest);
-    Future.delayed(const Duration(milliseconds: 400))
-        .then((value) => _applyRepository.getApplyList(GetApplyListRequest()));
-  }
+  Future<List<ApplyResponse>> execute(
+          {required ApplyCancelRequest applyCancelRequest}) =>
+      _applyRepository.applyCancel(applyCancelRequest: applyCancelRequest);
 }
