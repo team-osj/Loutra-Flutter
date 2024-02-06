94f83301d5091b5e4b025c3acd984b653fa6a11b
yoochanhong <chanhongy6@naver.com>
2024-02-06 20:00:57 +0900

    feat :: GetAllLaundryListEvent 핸들링

--- lib/presentation/laundry_room_page/bloc/laundry_bloc.dart
+++ lib/presentation/laundry_room_page/bloc/laundry_bloc.dart
@@ -1,15 +1,23 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lotura/data/dto/response/laundry_response.dart';
+import 'package:lotura/domain/use_case/get_all_laundry_list_use_case.dart';
 import 'package:lotura/domain/use_case/get_laundry_status_use_case.dart';
 import 'package:lotura/presentation/laundry_room_page/bloc/laundry_event.dart';
 import 'package:lotura/presentation/laundry_room_page/bloc/laundry_state.dart';
 
-class LaundryBloc extends Bloc<LaundryEvent, LaundryState> {
+class LaundryBloc
+    extends Bloc<LaundryEvent, LaundryState<List<LaundryResponse>>> {
   final GetLaundryStatusUseCase _getLaundryStatusUseCase;
+  final GetAllLaundryListUseCase _getAllLaundryListEventUseCase;
 
-  LaundryBloc({required GetLaundryStatusUseCase getLaundryStatusUseCase})
+  LaundryBloc(
+      {required GetLaundryStatusUseCase getLaundryStatusUseCase,
+      required GetAllLaundryListUseCase getAllLaundryListUseCase})
       : _getLaundryStatusUseCase = getLaundryStatusUseCase,
+        _getAllLaundryListEventUseCase = getAllLaundryListUseCase,
         super(Empty()) {
     on<GetLaundryEvent>(_getLaundryEventHandler);
+    on<GetAllLaundryListEvent>(_getAllLaundryListEventHandler);
   }
 
   void _getLaundryEventHandler(
@@ -17,11 +25,19 @@ class LaundryBloc extends Bloc<LaundryEvent, LaundryState> {
     try {
       _getLaundryStatusUseCase.execute();
       emit(Loading());
-      await for (var value in _getLaundryStatusUseCase.laundryList) {
-        emit(Loaded(laundryResponseList: value));
-      }
     } catch (e) {
-      emit(Error(message: e.toString()));
+      emit(Error(error: e));
+    }
+  }
+
+  void _getAllLaundryListEventHandler(
+      GetAllLaundryListEvent event, Emitter<LaundryState> emit) async {
+    try {
+      emit(Loading());
+      final laundryList = await _getAllLaundryListEventUseCase.execute();
+      emit(Loaded(data: laundryList));
+    } catch (e) {
+      emit(Error(error: e));
     }
   }
 }
