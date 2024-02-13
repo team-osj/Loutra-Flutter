a6765f87abbe27a37d2f1d886f81eb7e246f9995
yoochanhong <chanhongy6@naver.com>
2024-02-14 03:05:31 +0900

    feat :: foreground dispatch 적용

--- android/app/src/main/kotlin/com/osj/lotura/MainActivity.kt
+++ android/app/src/main/kotlin/com/osj/lotura/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.osj.lotura
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
 import android.nfc.NdefMessage
 import android.nfc.NdefRecord
 import android.nfc.NfcAdapter
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.KeyData.CHANNEL
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
     private lateinit var nfcAdapter: NfcAdapter
@@ -35,5 +41,42 @@ class MainActivity : FlutterActivity() {
                 }
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        if (intent.action == NfcAdapter.ACTION_NDEF_DISCOVERED) {
+            val data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            val message = data?.get(0) as NdefMessage
+            val record = message.records[0] as NdefRecord
+            val byteArr = record.payload
+
+            returnData = String(byteArr)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        enableNfcForegroundDispatch()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disableNfcForegroundDispatch()
+    }
+
+    private fun enableNfcForegroundDispatch() {
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val intentFilters = arrayOf(
+            IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED),
+        )
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, null)
+    }
+
+    private fun disableNfcForegroundDispatch() {
+        nfcAdapter.disableForegroundDispatch(this)
     }
 }
