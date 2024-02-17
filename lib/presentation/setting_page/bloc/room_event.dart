5942754cff53942706c0c3f7439a1aca4a8d3667
yoochanhong <chanhongy6@naver.com>
2024-02-17 16:55:10 +0900

    feat :: 세탁실 위치 수정 event 추가

--- lib/presentation/setting_page/bloc/room_event.dart
+++ lib/presentation/setting_page/bloc/room_event.dart
@@ -3,7 +3,13 @@ abstract class RoomEvent {}
 class GetRoomIndexEvent extends RoomEvent {}
 
 class UpdateRoomIndexEvent extends RoomEvent {
-  final int value;
+  final int roomIndex;
 
-  UpdateRoomIndexEvent({required this.value});
+  UpdateRoomIndexEvent({required this.roomIndex});
+}
+
+class ModifyRoomIndexEvent extends RoomEvent {
+  final int roomIndex;
+
+  ModifyRoomIndexEvent({required this.roomIndex});
 }
