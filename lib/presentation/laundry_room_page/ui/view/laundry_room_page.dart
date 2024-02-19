222bfdea85d5485d643f8bee1dc9c057e55779dd
yoochanhong <chanhongy6@naver.com>
2024-02-19 18:44:08 +0900

    refactor :: Loaded 상태일때 뷰 분리하기

--- lib/presentation/laundry_room_page/ui/view/laundry_room_page.dart
+++ lib/presentation/laundry_room_page/ui/view/laundry_room_page.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lotura/data/dto/response/laundry_response.dart';
@@ -79,32 +76,7 @@ class _LaundryRoomPageState extends State<LaundryRoomPage>
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addObserver(this);
     context.read<RoomBloc>().add(GetRoomIndexEvent());
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    WidgetsBinding.instance.removeObserver(this);
-  }
-
-  void func() {
-    const platformMsg = MethodChannel('com.osj.lotura/nfc_info');
-    platformMsg.invokeMethod("getNFCInfo").then((value) {
-      print("------------$value");
-      widget.nfcTagData = (jsonDecode(value))['index'] as int;
-    });
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    super.didChangeAppLifecycleState(state);
-    switch (state) {
-      case AppLifecycleState.resumed:
-        func();
-      default:
-    }
   }
 
   @override
@@ -242,141 +214,10 @@ class _LaundryRoomPageState extends State<LaundryRoomPage>
                             const Center(child: CircularProgressIndicator()),
                           Error() =>
                             Center(child: Text(state.error.toString())),
-                          Loaded() => ScrollConfiguration(
-                              behavior: const ScrollBehavior()
-                                  .copyWith(overscroll: false),
-                              child: ListView.builder(
-                                itemCount:
-                                    roomBlocState.value.roomIndex == 2 ? 10 : 8,
-                                itemBuilder: (context, index) {
-                                  Future.delayed(Duration.zero).then((value) {
-                                    if (widget.nfcTagData != -1 &&
-                                        roomBlocState
-                                                .value.isNFCShowBottomSheet ==
-                                            false) {
-                                      showModalBottomSheet(
-                                        context: context,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.vertical(
-                                            top: Radius.circular(25.r),
-                                          ),
-                                        ),
-                                        builder: (context) => OSJBottomSheet(
-                                          index: widget.nfcTagData,
-                                          isEnableNotification: true,
-                                          isWoman: widget.nfcTagData > 31
-                                              ? true
-                                              : false,
-                                          status: status[state
-                                              .data[widget.nfcTagData - 1]
-                                              .state],
-                                          machine: machine[state
-                                              .data[widget.nfcTagData - 1]
-                                              .deviceType],
-                                        ),
-                                      );
-                                      roomBlocState.value
-                                          .copyWith(isNFCShowBottomSheet: true);
-                                    }
-                                  });
-                                  return Column(
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          machineWidget(
-                                              roomState: roomBlocState.value,
-                                              index: state
-                                                  .data[placeIndex[roomBlocState
-                                                          .value.roomIndex] +
-                                                      index]
-                                                  .id,
-                                              machine: machine[state
-                                                  .data[placeIndex[roomBlocState
-                                                          .value.roomIndex] +
-                                                      index]
-                                                  .deviceType],
-                                              status: status[state
-                                                  .data[placeIndex[roomBlocState
-                                                          .value.roomIndex] +
-                                                      index]
-                                                  .state]),
-                                          triangle(
-                                              roomEntity: roomBlocState.value),
-                                          machineWidget(
-                                            roomState: roomBlocState.value,
-                                            index: placeIndex[roomBlocState
-                                                            .value.roomIndex] +
-                                                        index +
-                                                        (roomBlocState.value
-                                                                    .roomIndex ==
-                                                                2
-                                                            ? 10
-                                                            : 8) <
-                                                    44
-                                                ? state
-                                                    .data[placeIndex[
-                                                            roomBlocState.value
-                                                                .roomIndex] +
-                                                        index +
-                                                        (roomBlocState.value
-                                                                    .roomIndex ==
-                                                                2
-                                                            ? 10
-                                                            : 8)]
-                                                    .id
-                                                : -1,
-                                            machine: placeIndex[roomBlocState
-                                                            .value.roomIndex] +
-                                                        index +
-                                                        (roomBlocState.value
-                                                                    .roomIndex ==
-                                                                2
-                                                            ? 10
-                                                            : 8) <
-                                                    44
-                                                ? machine[state
-                                                    .data[placeIndex[
-                                                            roomBlocState.value
-                                                                .roomIndex] +
-                                                        index +
-                                                        (roomBlocState.value
-                                                                    .roomIndex ==
-                                                                2
-                                                            ? 10
-                                                            : 8)]
-                                                    .deviceType]
-                                                : Machine.dry,
-                                            status: placeIndex[roomBlocState
-                                                            .value.roomIndex] +
-                                                        index +
-                                                        (roomBlocState.value
-                                                                    .roomIndex ==
-                                                                2
-                                                            ? 10
-                                                            : 8) <
-                                                    44
-                                                ? status[state
-                                                    .data[placeIndex[
-                                                            roomBlocState.value
-                                                                .roomIndex] +
-                                                        index +
-                                                        (roomBlocState.value
-                                                                    .roomIndex ==
-                                                                2
-                                                            ? 10
-                                                            : 8)]
-                                                    .state]
-                                                : Status.breakdown,
-                                          ),
-                                        ],
-                                      ),
-                                      SizedBox(height: 10.0.h),
-                                    ],
-                                  );
-                                },
-                              ),
+                          Loaded() => LaundryList(
+                              list: state.data,
+                              roomEntity: roomBlocState.value,
+                              nfcData: widget.nfcTagData,
                             ),
                         };
                       },
@@ -390,6 +231,138 @@ class _LaundryRoomPageState extends State<LaundryRoomPage>
           return const Center(child: CircularProgressIndicator());
         }
       },
+    );
+  }
+}
+
+class LaundryList extends StatelessWidget {
+  LaundryList(
+      {super.key,
+      required this.list,
+      required this.roomEntity,
+      required this.nfcData});
+
+  final List<LaundryResponse> list;
+  final RoomEntity roomEntity;
+  final int nfcData;
+
+  final Map placeIndex = <int, int>{0: 0, 1: 16, 2: 31};
+
+  final Map status = <int, Status>{
+    0: Status.working,
+    1: Status.available,
+    2: Status.disconnected,
+    3: Status.breakdown
+  };
+
+  final Map machine = <String, Machine>{
+    "WASH": Machine.wash,
+    "DRY": Machine.dry
+  };
+
+  Widget triangle({required RoomEntity roomEntity}) =>
+      roomEntity.placeIconIndex == 0
+          ? const SizedBox.shrink()
+          : const Icon(LoturaIcons.triangleUp, color: Colors.grey);
+
+  MachineWidget machineWidget(
+          {required RoomEntity roomState,
+          required int index,
+          required Status status,
+          required Machine machine}) =>
+      roomState.placeIconIndex == 0
+          ? MachineCard(
+              index: index,
+              isEnableNotification: true,
+              isWoman: roomState.roomIndex == 2 ? true : false,
+              status: status,
+              machine: machine)
+          : MachineButton(
+              index: index,
+              isEnableNotification: true,
+              isWoman: roomState.roomIndex == 2 ? true : false,
+              status: status,
+              machine: machine);
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (nfcData != -1 && roomEntity.isNFCShowBottomSheet == false) {
+        context.read<RoomBloc>().add(ShowBottomSheetEvent());
+        showModalBottomSheet(
+          context: context,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(25.r),
+            ),
+          ),
+          builder: (context) => OSJBottomSheet(
+            index: nfcData,
+            isEnableNotification: true,
+            isWoman: nfcData > 31 ? true : false,
+            status: status[list[nfcData - 1].state],
+            machine: machine[list[nfcData - 1].deviceType],
+          ),
+        );
+      }
+    });
+    return ScrollConfiguration(
+      behavior: const ScrollBehavior().copyWith(overscroll: false),
+      child: ListView.builder(
+        itemCount: roomEntity.roomIndex == 2 ? 10 : 8,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  machineWidget(
+                      roomState: roomEntity,
+                      index: list[placeIndex[roomEntity.roomIndex] + index].id,
+                      machine: machine[
+                          list[placeIndex[roomEntity.roomIndex] + index]
+                              .deviceType],
+                      status: status[
+                          list[placeIndex[roomEntity.roomIndex] + index]
+                              .state]),
+                  triangle(roomEntity: roomEntity),
+                  machineWidget(
+                    roomState: roomEntity,
+                    index: placeIndex[roomEntity.roomIndex] +
+                                index +
+                                (roomEntity.roomIndex == 2 ? 10 : 8) <
+                            44
+                        ? list[placeIndex[roomEntity.roomIndex] +
+                                index +
+                                (roomEntity.roomIndex == 2 ? 10 : 8)]
+                            .id
+                        : -1,
+                    machine: placeIndex[roomEntity.roomIndex] +
+                                index +
+                                (roomEntity.roomIndex == 2 ? 10 : 8) <
+                            44
+                        ? machine[list[placeIndex[roomEntity.roomIndex] +
+                                index +
+                                (roomEntity.roomIndex == 2 ? 10 : 8)]
+                            .deviceType]
+                        : Machine.dry,
+                    status: placeIndex[roomEntity.roomIndex] +
+                                index +
+                                (roomEntity.roomIndex == 2 ? 10 : 8) <
+                            44
+                        ? status[list[placeIndex[roomEntity.roomIndex] +
+                                index +
+                                (roomEntity.roomIndex == 2 ? 10 : 8)]
+                            .state]
+                        : Status.breakdown,
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0.h),
+            ],
+          );
+        },
+      ),
     );
   }
 }
