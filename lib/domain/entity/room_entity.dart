621901c2208495ba70095e523970e7b49c0e53aa
yoochanhong <chanhongy6@naver.com>
2024-02-20 22:41:02 +0900

    refactor :: placeIconIndex 자료형 수정
    ButtonView enum을 채용함

--- lib/domain/entity/room_entity.dart
+++ lib/domain/entity/room_entity.dart
@@ -2,24 +2,24 @@ import 'package:lotura/main.dart';
 
 class RoomEntity {
   final RoomLocation roomLocation;
-  final int placeIconIndex;
+  final ButtonView buttonView;
   final bool isClick, isNFCShowBottomSheet;
 
   const RoomEntity({
     required this.roomLocation,
-    required this.placeIconIndex,
+    required this.buttonView,
     required this.isClick,
     required this.isNFCShowBottomSheet,
   });
 
   RoomEntity copyWith(
       {RoomLocation? roomLocation,
-      int? placeIconIndex,
+      ButtonView? buttonView,
       bool? isClick,
       bool? isNFCShowBottomSheet}) {
     return RoomEntity(
         roomLocation: roomLocation ?? this.roomLocation,
-        placeIconIndex: placeIconIndex ?? this.placeIconIndex,
+        buttonView: buttonView ?? this.buttonView,
         isClick: isClick ?? this.isClick,
         isNFCShowBottomSheet:
             isNFCShowBottomSheet ?? this.isNFCShowBottomSheet);
