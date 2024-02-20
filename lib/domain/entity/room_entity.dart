151badcb913be7e3fe890b2f4345126371a349d4
yoochanhong <chanhongy6@naver.com>
2024-02-21 01:26:21 +0900

    style :: 클래스명 변경
    RoomEntity => LaundryRoomEntity

--- lib/domain/entity/room_entity.dart
+++ lib/domain/entity/room_entity.dart
@@ -1,23 +1,23 @@
 import 'package:lotura/main.dart';
 
-class RoomEntity {
+class LaundryRoomEntity {
   final RoomLocation roomLocation;
   final ButtonView buttonView;
   final bool isClick, isNFCShowBottomSheet;
 
-  const RoomEntity({
+  const LaundryRoomEntity({
     required this.roomLocation,
     required this.buttonView,
     required this.isClick,
     required this.isNFCShowBottomSheet,
   });
 
-  RoomEntity copyWith(
+  LaundryRoomEntity copyWith(
       {RoomLocation? roomLocation,
       ButtonView? buttonView,
       bool? isClick,
       bool? isNFCShowBottomSheet}) {
-    return RoomEntity(
+    return LaundryRoomEntity(
         roomLocation: roomLocation ?? this.roomLocation,
         buttonView: buttonView ?? this.buttonView,
         isClick: isClick ?? this.isClick,
