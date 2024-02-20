de44b7cb55312c03f5da8493041e20de0a535b80
yoochanhong <chanhongy6@naver.com>
2024-02-21 02:25:25 +0900

    refactor :: LaundryBloc에 LaundryEntity 적용

--- lib/presentation/laundry_room_page/bloc/laundry_bloc.dart
+++ lib/presentation/laundry_room_page/bloc/laundry_bloc.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:lotura/data/dto/response/laundry_response.dart';
+import 'package:lotura/domain/entity/laundry_entity.dart';
 import 'package:lotura/domain/use_case/get_all_laundry_list_use_case.dart';
 import 'package:lotura/domain/use_case/get_laundry_status_use_case.dart';
 import 'package:lotura/presentation/laundry_room_page/bloc/laundry_event.dart';
 import 'package:lotura/presentation/laundry_room_page/bloc/laundry_state.dart';
 
 class LaundryBloc
-    extends Bloc<LaundryEvent, LaundryState<List<LaundryResponse>>> {
+    extends Bloc<LaundryEvent, LaundryState<List<LaundryEntity>>> {
   final GetLaundryStatusUseCase _getLaundryStatusUseCase;
   final GetAllLaundryListUseCase _getAllLaundryListEventUseCase;
 
@@ -21,17 +21,17 @@ class LaundryBloc
   }
 
   void _getLaundryEventHandler(GetLaundryEvent event,
-      Emitter<LaundryState<List<LaundryResponse>>> emit) async {
+      Emitter<LaundryState<List<LaundryEntity>>> emit) async {
     try {
       _getLaundryStatusUseCase.execute();
       await for (var data in _getLaundryStatusUseCase.laundryList) {
         final newState = Loaded(
-            data: state.valueOrNull!
+            data: state.value
                 .map((e) => e.id == data.id
-                    ? LaundryResponse(
+                    ? LaundryEntity(
                         id: data.id,
-                        state: data.state,
-                        deviceType: data.deviceType)
+                        state: data.state.index,
+                        deviceType: data.deviceType.name.toUpperCase())
                     : e)
                 .toList());
         emit(newState);
@@ -42,7 +42,7 @@ class LaundryBloc
   }
 
   void _getAllLaundryListEventHandler(GetAllLaundryListEvent event,
-      Emitter<LaundryState<List<LaundryResponse>>> emit) async {
+      Emitter<LaundryState<List<LaundryEntity>>> emit) async {
     try {
       emit(Loading());
       final newState =
