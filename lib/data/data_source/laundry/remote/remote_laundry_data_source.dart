8f5ae18642b9c8adc9a75e08bb26f6d16fc610ab
yoochanhong <chanhongy6@naver.com>
2024-02-21 02:05:13 +0900

    refactor :: toEntity 메소드 적용

--- lib/data/data_source/laundry/remote/remote_laundry_data_source.dart
+++ lib/data/data_source/laundry/remote/remote_laundry_data_source.dart
@@ -3,17 +3,18 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:lotura/data/dto/response/laundry_response.dart';
+import 'package:lotura/domain/entity/laundry_entity.dart';
 import 'package:lotura/secret.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class RemoteLaundryDataSource {
-  final StreamController<LaundryResponse> _streamController;
+  final StreamController<LaundryEntity> _streamController;
 
   RemoteLaundryDataSource(
-      {required StreamController<LaundryResponse> streamController})
+      {required StreamController<LaundryEntity> streamController})
       : _streamController = streamController;
 
-  Stream<LaundryResponse> get laundryList =>
+  Stream<LaundryEntity> get laundryList =>
       _streamController.stream.asBroadcastStream();
 
   void webSocketInit() async {
@@ -21,17 +22,18 @@ class RemoteLaundryDataSource {
     await channel.ready;
     channel.stream.listen((data) {
       _streamController.sink.add(
-          LaundryResponse.fromJson(jsonDecode(data) as Map<String, dynamic>));
+          LaundryResponse.fromJson(jsonDecode(data) as Map<String, dynamic>)
+              .toEntity());
     });
   }
 
-  Future<List<LaundryResponse>> getAllLaundryList() async {
+  Future<List<LaundryEntity>> getAllLaundryList() async {
     final response = await http.get(Uri.parse("$baseurl/device_list"));
     if (response.statusCode != 200) {
       throw Exception(response.body);
     }
     return (jsonDecode(response.body) as List<dynamic>)
-        .map((i) => LaundryResponse.fromJson(i))
+        .map((i) => LaundryResponse.fromJson(i).toEntity())
         .toList();
   }
 }
