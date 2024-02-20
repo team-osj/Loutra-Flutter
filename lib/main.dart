942e6f657ec96da5ff8b5d0edf681204dbe5b469
yoochanhong <chanhongy6@naver.com>
2024-02-20 17:54:04 +0900

    refactor :: required 적용

--- lib/main.dart
+++ lib/main.dart
@@ -68,11 +68,11 @@ enum Status {
 enum Machine { wash, dry }
 
 enum RoomLocation {
-  schoolSide("남자 학교측 세탁실"),
-  dormitorySide("남자 기숙사측 세탁실"),
-  womanRoom("여자 세탁실");
+  schoolSide(roomName: "남자 학교측 세탁실"),
+  dormitorySide(roomName: "남자 기숙사측 세탁실"),
+  womanRoom(roomName: "여자 세탁실");
 
-  const RoomLocation(this.roomName);
+  const RoomLocation({required this.roomName});
 
   final String roomName;
 }
