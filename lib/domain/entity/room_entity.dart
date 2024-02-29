b4a0a7d721ca16a4e38b85c1ed3121c769dbb496
yoochanhong <chanhongy6@naver.com>
2024-02-29 23:14:37 +0900

    feat :: 바텀시트 생존 여부 필드 추가

--- lib/domain/entity/room_entity.dart
+++ lib/domain/entity/room_entity.dart
@@ -3,25 +3,27 @@ import 'package:lotura/main.dart';
 class LaundryRoomEntity {
   final RoomLocation roomLocation;
   final ButtonView buttonView;
-  final bool isClick, isNFCShowBottomSheet;
+  final bool isClick, isNFCShowBottomSheet, showingBottomSheet;
 
   const LaundryRoomEntity({
     required this.roomLocation,
     required this.buttonView,
     required this.isClick,
     required this.isNFCShowBottomSheet,
+    required this.showingBottomSheet,
   });
 
   LaundryRoomEntity copyWith(
       {RoomLocation? roomLocation,
       ButtonView? buttonView,
       bool? isClick,
-      bool? isNFCShowBottomSheet}) {
+      bool? isNFCShowBottomSheet,
+      bool? showingBottomSheet}) {
     return LaundryRoomEntity(
         roomLocation: roomLocation ?? this.roomLocation,
         buttonView: buttonView ?? this.buttonView,
         isClick: isClick ?? this.isClick,
-        isNFCShowBottomSheet:
-            isNFCShowBottomSheet ?? this.isNFCShowBottomSheet);
+        isNFCShowBottomSheet: isNFCShowBottomSheet ?? this.isNFCShowBottomSheet,
+        showingBottomSheet: showingBottomSheet ?? this.showingBottomSheet);
   }
 }
