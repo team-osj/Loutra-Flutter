7fc0639aceea86cf4d63aa56b4b4ad497fccf4ea
yoochanhong <chanhongy6@naver.com>
2024-03-01 22:24:32 +0900

    feat :: 앱 버전 비교 로직 추가

--- lib/presentation/splash_page/ui/view/splash_page.dart
+++ lib/presentation/splash_page/ui/view/splash_page.dart
@@ -1,15 +1,19 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lotura/data/dto/request/get_apply_list_request.dart';
+import 'package:lotura/presentation/app_update_page/ui/app_update_page.dart';
 import 'package:lotura/presentation/apply_page/bloc/apply_bloc.dart';
 import 'package:lotura/presentation/apply_page/bloc/apply_event.dart';
 import 'package:lotura/presentation/laundry_room_page/bloc/laundry_bloc.dart';
 import 'package:lotura/presentation/laundry_room_page/bloc/laundry_event.dart';
 import 'package:lotura/presentation/utils/bottom_navi.dart';
 import 'package:lotura/presentation/utils/lotura_colors.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key, required this.nfcTagData});
@@ -21,24 +25,45 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  Future<void> checkAppVersion() async {
+    final remoteConfig = FirebaseRemoteConfig.instance;
+    await remoteConfig.fetchAndActivate();
+
+    String firebaseVersion = remoteConfig.getString(
+        Platform.isAndroid ? "ANDROID_APP_VERSION" : "iOS_APP_VERSION");
+
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    String appVersion = packageInfo.version;
+
+    if (firebaseVersion != appVersion) {
+      Future.delayed(Duration.zero).then((value) =>
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => const AppUpdatePage()),
+              (route) => false));
+    } else {
+      Future.delayed(const Duration(seconds: 1)).then(
+        (value) {
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                builder: (context) => BottomNavi(nfcTagData: widget.nfcTagData),
+              ),
+              (route) => false);
+        },
+      );
+    }
+  }
+
   @override
   void initState() {
     super.initState();
+    checkAppVersion();
     context.read<LaundryBloc>().add(GetAllLaundryListEvent());
     context.read<LaundryBloc>().add(GetLaundryEvent());
     context
         .read<ApplyBloc>()
         .add(GetApplyListEvent(getApplyListRequest: GetApplyListRequest()));
-    Future.delayed(const Duration(milliseconds: 1100)).then(
-      (value) {
-        Navigator.pushAndRemoveUntil(
-            context,
-            MaterialPageRoute(
-              builder: (context) => BottomNavi(nfcTagData: widget.nfcTagData),
-            ),
-            (route) => false);
-      },
-    );
   }
 
   @override
