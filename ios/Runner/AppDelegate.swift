f46b551b9d9c6ccef7f9490acb60aee28d9882fa
yoochanhong <chanhongy6@naver.com>
2024-03-14 16:14:43 +0900

    feat :: iOS NFC 감지 및 메소드 채널링 기능 구현

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,51 @@
 import UIKit
 import Flutter
 
+@available(iOS 13.0.0, *)
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    private var channel: FlutterMethodChannel?
+    private var nfcData = -1
+    private var isAppRunning = false
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        let controller = self.window.rootViewController as! FlutterViewController
+        channel = FlutterMethodChannel.init(name: "com.osj.lotura/nfc_info", binaryMessenger: controller as! FlutterBinaryMessenger)
+        channel?.setMethodCallHandler { (call, result) in
+            if(call.method == "getNFCInfo") {
+                result("{\"index\" : \(self.nfcData)}")
+            }
+        }
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    override func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        let contents = url.absoluteString.components(separatedBy: "=")
+        self.nfcData = NSString(string : contents[1]).integerValue
+        return true
+    }
+    
+    override func applicationWillEnterForeground(_ application: UIApplication) {
+        if self.isAppRunning == true {
+            self.nfcData = -1
+        } else {
+            self.isAppRunning = true
+        }
+    }
+    
+    override func applicationWillResignActive(_ application: UIApplication) {
+        if self.isAppRunning == true {
+            self.nfcData = -1
+        } else {
+            self.isAppRunning = true
+        }
+    }
 }
