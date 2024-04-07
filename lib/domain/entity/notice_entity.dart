293f64570a1d1e4a3c743e85ccda35b50f500bb7
yoochanhong <chanhongy6@naver.com>
2024-04-08 08:46:31 +0900

    feat :: 공지 엔티티

---
+++ lib/domain/entity/notice_entity.dart
@@ -0,0 +1,11 @@
+class NoticeEntity {
+  final String title;
+  final String contents;
+  final String date;
+
+  const NoticeEntity({
+    required this.title,
+    required this.contents,
+    required this.date,
+  });
+}
