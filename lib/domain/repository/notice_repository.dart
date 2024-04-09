2af8e14a4a1e92ace0b2e86b69f6d59811723344
yoochanhong <chanhongy6@naver.com>
2024-04-09 17:14:30 +0900

    feat :: notice repository 메소드 추가

--- lib/domain/repository/notice_repository.dart
+++ lib/domain/repository/notice_repository.dart
@@ -4,4 +4,8 @@ import 'package:lotura/domain/entity/notice_entity.dart';
 
 abstract class NoticeRepository {
   Future<List<NoticeEntity>> getNotice();
+
+  Future<void> setLastNoticeId({required String key, required int value});
+
+  int? getLastNoticeId({required String key});
 }
