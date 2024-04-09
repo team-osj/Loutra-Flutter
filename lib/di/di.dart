2d8b795b0d47ebfe008f263386c3615ddcdf6c7e
yoochanhong <chanhongy6@naver.com>
2024-04-09 18:53:51 +0900

    feat :: local notion data source 의존성 주입

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -5,6 +5,7 @@ import 'package:hive/hive.dart';
 import 'package:lotura/data/data_source/apply/remote/remote_apply_data_source.dart';
 import 'package:lotura/data/data_source/laundry/local/local_laundry_data_source.dart';
 import 'package:lotura/data/data_source/laundry/remote/remote_laundry_data_source.dart';
+import 'package:lotura/data/data_source/notice/local/local_notice_data_source.dart';
 import 'package:lotura/data/data_source/notice/remote/remote_notice_data_source.dart';
 import 'package:lotura/data/repository/apply_repository_impl.dart';
 import 'package:lotura/data/repository/laundry_repository_impl.dart';
@@ -16,10 +17,12 @@ import 'package:lotura/domain/repository/notice_repository.dart';
 import 'package:lotura/domain/use_case/apply_cancel_use_case.dart';
 import 'package:lotura/domain/use_case/get_all_laundry_list_use_case.dart';
 import 'package:lotura/domain/use_case/get_apply_list_use_case.dart';
+import 'package:lotura/domain/use_case/get_last_notice_id_use_case.dart';
 import 'package:lotura/domain/use_case/get_laundry_room_index_use_case.dart';
 import 'package:lotura/domain/use_case/get_laundry_status_use_case.dart';
 import 'package:lotura/domain/use_case/get_notice_use_case.dart';
 import 'package:lotura/domain/use_case/send_fcm_info_use_case.dart';
+import 'package:lotura/domain/use_case/update_last_notice_id_use_case.dart';
 import 'package:lotura/domain/use_case/update_laundry_room_index_use_case.dart';
 import 'package:lotura/presentation/apply_page/bloc/apply_bloc.dart';
 import 'package:lotura/presentation/laundry_room_page/bloc/laundry_bloc.dart';
@@ -32,10 +35,13 @@ Future<List<BlocProvider>> di() async {
   LocalLaundryDataSource localLaundryDataSource =
       LocalLaundryDataSource(localDatabase: box);
 
+  LocalNoticeDataSource localNoticeDataSource = LocalNoticeDataSource(box: box);
+
   RemoteLaundryDataSource remoteLaundryDataSource = RemoteLaundryDataSource(
       streamController: StreamController<LaundryEntity>.broadcast());
 
   RemoteApplyDataSource remoteApplyDataSource = RemoteApplyDataSource();
+
   RemoteNoticeDataSource remoteNoticeDataSource = RemoteNoticeDataSource();
 
   LaundryRepository laundryRepository = LaundryRepositoryImpl(
@@ -45,8 +51,9 @@ Future<List<BlocProvider>> di() async {
   ApplyRepository applyRepository =
       ApplyRepositoryImpl(remoteApplyDataSource: remoteApplyDataSource);
 
-  NoticeRepository noticeRepository =
-      NoticeRepositoryImpl(remoteNoticeDataSource: remoteNoticeDataSource);
+  NoticeRepository noticeRepository = NoticeRepositoryImpl(
+      remoteNoticeDataSource: remoteNoticeDataSource,
+      localNoticeDataSource: localNoticeDataSource);
 
   GetLaundryStatusUseCase getLaundryStatusUseCase =
       GetLaundryStatusUseCase(laundryRepository: laundryRepository);
@@ -72,6 +79,12 @@ Future<List<BlocProvider>> di() async {
   GetNoticeUseCase getNoticeUseCase =
       GetNoticeUseCase(noticeRepository: noticeRepository);
 
+  GetLastNoticeIdUseCase getLastNoticeIdUseCase =
+      GetLastNoticeIdUseCase(noticeRepository: noticeRepository);
+
+  UpdateLastNoticeIdUseCase updateLastNoticeIdUseCase =
+      UpdateLastNoticeIdUseCase(noticeRepository: noticeRepository);
+
   return [
     BlocProvider<ApplyBloc>(
         create: (context) => ApplyBloc(
@@ -88,7 +101,11 @@ Future<List<BlocProvider>> di() async {
           updateLaundryRoomIndexUseCase: updateLaundryRoomIndexUseCase),
     ),
     BlocProvider<NoticeBloc>(
-      create: (context) => NoticeBloc(getNoticeUseCase: getNoticeUseCase),
+      create: (context) => NoticeBloc(
+        getNoticeUseCase: getNoticeUseCase,
+        getLastNoticeIdUseCase: getLastNoticeIdUseCase,
+        updateLastNoticeIdUseCase: updateLastNoticeIdUseCase,
+      ),
     ),
   ];
 }
