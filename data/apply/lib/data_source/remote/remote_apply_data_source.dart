4b14571242c2525b8cf1ec64dfe2115c768c256a
yoochanhong <chanhongy6@naver.com>
2024-04-28 20:34:00 +0900

    chore :: firebase messing 패키지 제거

--- data/apply/lib/data_source/remote/remote_apply_data_source.dart
+++ data/apply/lib/data_source/remote/remote_apply_data_source.dart
@@ -6,16 +6,15 @@ import 'package:apply_data/dto/request/get_apply_list_request.dart';
 import 'package:apply_data/dto/request/send_fcm_info_request.dart';
 import 'package:apply_data/dto/response/apply_response.dart';
 import 'package:core/secret.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:http/http.dart' as http;
 
 class RemoteApplyDataSource {
-  Future<String> _getToken() async =>
-      await FirebaseMessaging.instance.getToken() ?? "whatThe";
+  // Future<String> _getToken() async =>
+  //     await FirebaseMessaging.instance.getToken() ?? "whatThe";
 
   Future<List<ApplyResponse>> getApplyList() async {
     GetApplyListRequest getApplyListRequest =
-        GetApplyListRequest(token: await _getToken());
+        GetApplyListRequest(token: "asdf");
     final response = await http.post(Uri.parse("$baseurl/push_list"),
         body: json.encode(getApplyListRequest.toJson()));
     if (response.statusCode != 200) throw Exception(response.body);
@@ -27,7 +26,7 @@ class RemoteApplyDataSource {
   Future<void> sendFCMInfo(
       {required int deviceId, required int expectState}) async {
     SendFCMInfoRequest sendFCMInfoRequest = SendFCMInfoRequest(
-        deviceId: deviceId, expectState: expectState, token: await _getToken());
+        deviceId: deviceId, expectState: expectState, token: "asdf");
     final response = await http.post(Uri.parse("$baseurl/push_request"),
         headers: {"Content-Type": "application/json"},
         body: json.encode(sendFCMInfoRequest.toJson()));
@@ -38,7 +37,7 @@ class RemoteApplyDataSource {
 
   Future<void> applyCancel({required int deviceId}) async {
     ApplyCancelRequest applyCancelRequest =
-        ApplyCancelRequest(deviceId: deviceId, token: await _getToken());
+        ApplyCancelRequest(deviceId: deviceId, token: "asdf");
     final response = await http.post(Uri.parse("$baseurl/push_cancel"),
         headers: {"Content-Type": "application/json"},
         body: json.encode(applyCancelRequest.toJson()));
