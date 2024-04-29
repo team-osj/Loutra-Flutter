3bec14d81ac934acb5357eadccd10911e03e4313
yoochanhong <chanhongy6@naver.com>
2024-04-29 20:22:21 +0900

    feat :: 공지 페이지 이동 아이콘 추가

--- presentation/laundry_room/lib/ui/view/laundry_room_page.dart
+++ presentation/laundry_room/lib/ui/view/laundry_room_page.dart
@@ -1,4 +1,8 @@
+import 'package:core/button_view.dart';
+import 'package:core/current_state.dart';
+import 'package:core/device_type.dart';
 import 'package:core/lotura_colors.dart';
+import 'package:core/lotura_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart' as s;
@@ -6,6 +10,12 @@ import 'package:laundry_domain/entity/laundry_entity.dart';
 import 'package:laundry_room_presentation/bloc/laundry_bloc.dart';
 import 'package:laundry_room_presentation/bloc/laundry_model.dart';
 import 'package:laundry_room_presentation/bloc/laundry_state.dart';
+import 'package:notice_presentation/bloc/notice_bloc.dart';
+import 'package:notice_presentation/bloc/notice_event.dart';
+import 'package:notice_presentation/ui/view/notice_page.dart';
+import 'package:utils/machine_button.dart';
+import 'package:utils/machine_widget.dart';
+import 'package:utils/osj_text_button.dart';
 
 class LaundryRoomPage extends StatelessWidget {
   LaundryRoomPage({super.key});
@@ -29,7 +39,7 @@ class LaundryRoomPage extends StatelessWidget {
         title: Padding(
           padding: EdgeInsets.only(left: 10.r),
           child: Text(
-            "ㅇㅇㅋㅋ",
+            "남자 학교측",
             style: TextStyle(
               color: LoturaColors.black,
               fontSize: 22.0.sp,
@@ -37,6 +47,21 @@ class LaundryRoomPage extends StatelessWidget {
             ),
           ),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<NoticeBloc>().add(UpdateLastNoticeIdEvent());
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const NoticePage()));
+            },
+            icon: Icon(
+              LoturaIcons.notice,
+              color: LoturaColors.black,
+              size: 24.0.r,
+            ),
+          ),
+          SizedBox(width: 24.0.w),
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.only(
@@ -46,6 +71,41 @@ class LaundryRoomPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.symmetric(vertical: 12.0.h),
+              child: Row(
+                children: [
+                  OSJTextButton(
+                    fontSize: 18.0.sp,
+                    color: LoturaColors.white,
+                    fontColor: LoturaColors.primary700,
+                    text: "남자 학교측",
+                    padding: EdgeInsets.symmetric(horizontal: 5.0.r),
+                    radius: 8.0,
+                  ),
+                  SizedBox(width: 8.0.w),
+                  OSJTextButton(
+                    fontSize: 18.0.sp,
+                    color: LoturaColors.gray100,
+                    fontColor: LoturaColors.gray300,
+                    text: "남자 기숙사측",
+                    padding: EdgeInsets.symmetric(horizontal: 5.0.r),
+                    radius: 8.0,
+                  ),
+                  SizedBox(width: 8.0.w),
+                  OSJTextButton(
+                    fontSize: 18.0.sp,
+                    color: LoturaColors.gray100,
+                    fontColor: LoturaColors.gray300,
+                    text: "여자",
+                    padding: EdgeInsets.symmetric(horizontal: 5.0.r),
+                    radius: 8.0,
+                  ),
+                ],
+              ),
+            ),
+            const ViewTypeFilter(buttonView: ButtonView.icon),
             Expanded(
               child: BlocBuilder<LaundryBloc, LaundryState<LaundryModel>>(
                 builder: (context, state) {
@@ -68,6 +128,39 @@ class LaundryRoomPage extends StatelessWidget {
   }
 }
 
+class ViewTypeFilter extends StatelessWidget {
+  const ViewTypeFilter({super.key, required this.buttonView});
+
+  final ButtonView buttonView;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(LoturaIcons.grid,
+              size: 18.0.r,
+              color: buttonView == ButtonView.image
+                  ? LoturaColors.black
+                  : LoturaColors.gray300),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            LoturaIcons.list,
+            size: 18.0.r,
+            color: buttonView == ButtonView.icon
+                ? LoturaColors.black
+                : LoturaColors.gray300,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class LaundryList extends StatelessWidget {
   LaundryList({
     super.key,
@@ -77,6 +170,17 @@ class LaundryList extends StatelessWidget {
   final List<LaundryEntity> list;
 
   final Map<int, int> placeIndex = {0: 0, 1: 16, 2: 31};
+
+  MachineWidget machineWidget(
+          {required int deviceId,
+          required CurrentState state,
+          required DeviceType deviceType}) =>
+      MachineButton(
+          deviceId: deviceId,
+          isEnableNotification: true,
+          isWoman: false,
+          state: state,
+          deviceType: deviceType);
 
   @override
   Widget build(BuildContext context) {
@@ -90,16 +194,14 @@ class LaundryList extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  )
+                  machineWidget(
+                      deviceId: list[index].id,
+                      deviceType: list[index].deviceType,
+                      state: list[index].state),
+                  machineWidget(
+                      deviceId: list[index + 8].id,
+                      deviceType: list[index + 8].deviceType,
+                      state: list[index + 8].state),
                 ],
               ),
               SizedBox(height: 10.0.h),
